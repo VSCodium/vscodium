6825d14395a05a399159d1c76cf5570aa86c7dba
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-08-14 10:50:44 -0400

    Binary releases of VS Code without MS branding/telemetry/licensing

--- check_tags.sh
+++ check_tags.sh
@@ -5,14 +5,8 @@ then
     echo "Latest MS tag ${LATEST_MS_TAG} already exists in VSCodium. Bail"
 else
     echo "New MS tag found, continuing build"
-    if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
-      echo $LATEST_MS_TAG > version.md
-      git config --local user.name "Travis CI"
-      git config --local user.email "builds@travis-ci.com"
-      git add version.md
-      git commit -m "${LATEST_MS_TAG}"
-      git tag $LATEST_MS_TAG
-      git push --quiet https://$GITHUB_TOKEN@github.com/VSCodium/vscodium master --tags > /dev/null 2>&1
-    fi
+    git config --local user.name "Travis CI"
+    git config --local user.email "builds@travis-ci.com"
+    git tag $LATEST_MS_TAG
     export SHOULD_BUILD="yes"
 fi
