606d3f8ff26474889a1e0f7bafa0a497c644eb85
Ruslan Gainutdinov <contact@ruslan.org>
2018-09-20 16:32:49 +0300

    If vscode exists, fetch updates (#19)

--- get_repo.sh
+++ get_repo.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-git clone https://github.com/Microsoft/vscode.git
-cd vscode
+if [ -d vscode ]; then
+  cd vscode 
+  git fetch --all
+else
+  git clone https://github.com/Microsoft/vscode.git
+  cd vscode
+fi
+
 export LATEST_MS_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo "Got the latest MS tag: ${LATEST_MS_TAG}"
 git checkout $LATEST_MS_TAG
