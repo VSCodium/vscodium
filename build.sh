01293ca451d78d36a0910758e44da8fe6331495e
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-10-02 11:41:05 -0400

    Undo telemetry before build

--- build.sh
+++ build.sh
@@ -9,6 +9,11 @@ if [[ "$SHOULD_BUILD" == "yes" ]]; then
   export NODE_ENV=production
 
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    # remove dc.services.visualstudio.com tracking
+    grep -rl --exclude-dir=.git "dc.services.visualstudio.com" . | xargs sed -i '' 's/dc\.services\.visualstudio\.com/0\.0\.0\.0/g'
+    # remove vortex.data.microsoft.com tracking
+    grep -rl --exclude-dir=.git "vortex.data.microsoft.com" . | xargs sed -i '' 's/vortex\.data\.microsoft\.com/0\.0\.0\.0/g'
+
     npx gulp vscode-darwin-min
   else
     # microsoft adds their apt repo to sources
@@ -16,6 +21,12 @@ if [[ "$SHOULD_BUILD" == "yes" ]]; then
     # as we are renaming the application to vscodium
     # we need to edit a line in the post install template
     sed -i "s/code-oss/vscodium/" resources/linux/debian/postinst.template
+
+    # delete dc.services.visualstudio.com tracking
+    grep -rl --exclude-dir=.git "dc.services.visualstudio.com" . | xargs sed -i 's/dc\.services\.visualstudio\.com/0\.0\.0\.0/g'
+    # remove vortex.data.microsoft.com tracking
+    grep -rl --exclude-dir=.git "vortex.data.microsoft.com" . | xargs sed -i 's/vortex\.data\.microsoft\.com/0\.0\.0\.0/g'
+
     npx gulp vscode-linux-x64-min
     npx gulp vscode-linux-x64-build-deb
     npx gulp vscode-linux-x64-build-rpm
