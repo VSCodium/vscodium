a9dfb472a5f56e509b737835ad2085d752636dfe
Your Name <you@example.com>
2018-10-09 17:12:21 +0000

    Add arm64 build commands

--- install_deps.sh
+++ install_deps.sh
@@ -5,6 +5,10 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew install yarn --without-node
   brew install jq zip
 else
+  # Use the default C / C++ compilers,
+  # because some makefiles default to CC:=gcc:
+  export CC=/usr/bin/cc
+  export CXX=/usr/bin/c++
   sudo apt-get update
   sudo apt-get install libx11-dev libxkbfile-dev libsecret-1-dev fakeroot rpm
   if [[ "$BUILDARCH" == "ia32" ]]; then
