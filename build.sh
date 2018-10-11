73e3c2e92aca3689dc2df2b3361164564cbb5b1c
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-10-11 11:53:32 -0400

    Clean up logic in build.sh

--- build.sh
+++ build.sh
@@ -16,12 +16,26 @@ if [[ "$SHOULD_BUILD" == "yes" ]]; then
 
   export NODE_ENV=production
 
-  if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
+  if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     # microsoft adds their apt repo to sources
     # unless the app name is code-oss
     # as we are renaming the application to vscodium
     # we need to edit a line in the post install template
     sed -i "s/code-oss/vscodium/" resources/linux/debian/postinst.template
+    if [[ "$BUILDARCH" == "ia32" ]]; then
+      npm run gulp vscode-linux-ia32-min
+      npm run gulp vscode-linux-ia32-build-deb
+      npm run gulp vscode-linux-ia32-build-rpm
+      unset npm_config_arch
+    elif [[ "$BUILDARCH" == "arm64" ]]; then
+      npm run gulp vscode-linux-arm64-min
+      npm run gulp vscode-linux-arm64-build-deb
+      # npm run gulp vscode-linux-arm64-build-rpm
+    else
+      npm run gulp vscode-linux-x64-min
+      npm run gulp vscode-linux-x64-build-deb
+      npm run gulp vscode-linux-x64-build-rpm
+    fi
   fi
 
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
@@ -33,19 +47,6 @@ if [[ "$SHOULD_BUILD" == "yes" ]]; then
     npx gulp vscode-win32-x64-system-setup
     npx gulp vscode-win32-x64-user-setup
     npx gulp vscode-win32-x64-archive
-  elif [[ "$BUILDARCH" == "ia32" ]]; then
-    npm run gulp vscode-linux-ia32-min
-    npm run gulp vscode-linux-ia32-build-deb
-    npm run gulp vscode-linux-ia32-build-rpm
-    unset npm_config_arch
-  elif [[ "$BUILDARCH" == "arm64" ]]; then
-    npm run gulp vscode-linux-arm64-min
-    npm run gulp vscode-linux-arm64-build-deb
-    # npm run gulp vscode-linux-arm64-build-rpm
-  else
-    npm run gulp vscode-linux-x64-min
-    npm run gulp vscode-linux-x64-build-deb
-    npm run gulp vscode-linux-x64-build-rpm
   fi
 
   cd ..
