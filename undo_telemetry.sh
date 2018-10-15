e7c39f3558dfeff3160579ad91b08b575ed35b6c
Joel Wasserman <joel.wasserman8@gmail.com>
2018-10-15 10:43:26 -0400

    Replace telemetry default true values in settings.json

--- undo_telemetry.sh
+++ undo_telemetry.sh
@@ -8,3 +8,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 else
   grep -rl --exclude-dir=.git -E $TELEMETRY_URLS . | xargs sed -i -E $REPLACEMENT
 fi
+
+# set defaults for telemetry reporting to false
+sed -i "s/\'default\': true,/\'default\': false,/g" ./src/vs/platform/telemetry/common/telemetryService.ts
+sed -i "s/\'default\': true,/\'default\': false,/g" ./src/vs/workbench/services/crashReporter/electron-browser/crashReporterService.ts
