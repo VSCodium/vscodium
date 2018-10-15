c85389894e35d0ab1373abe7719f533fdad73a90
Joel Wasserman <joel.wasserman8@gmail.com>
2018-10-15 15:52:55 -0400

    Added custom bash script to update settings pre yarn

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ if [[ "$SHOULD_BUILD" == "yes" ]]; then
     export npm_config_arch=ia32
   fi
 
+  ../update_settings.sh
+
   yarn
   mv product.json product.json.bak
   cat product.json.bak | jq 'setpath(["extensionsGallery"]; {"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery", "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index", "itemUrl": "https://marketplace.visualstudio.com/items"}) | setpath(["nameShort"]; "VSCodium") | setpath(["nameLong"]; "VSCodium") | setpath(["applicationName"]; "vscodium") | setpath(["win32MutexName"]; "vscodium") | setpath(["win32DirName"]; "VSCodium") | setpath(["win32NameVersion"]; "VSCodium") | setpath(["win32RegValueName"]; "VSCodium") | setpath(["win32AppUserModelId"]; "Microsoft.VSCodium") | setpath(["win32ShellNameShort"]; "V&SCodium") | setpath(["urlProtocol"]; "vscodium")' > product.json
