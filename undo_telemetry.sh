7b30ced395d9ad6f5e9160f973ed675308186148
Joel Wasserman <joel.wasserman8@gmail.com>
2018-10-15 13:56:12 -0400

    Added in sed argument for telemetry replace

--- undo_telemetry.sh
+++ undo_telemetry.sh
@@ -5,10 +5,13 @@ REPLACEMENT="s/$TELEMETRY_URLS/0\.0\.0\.0/g"
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   grep -rl --exclude-dir=.git -E $TELEMETRY_URLS . | xargs sed -i '' -E $REPLACEMENT
+  # set defaults for telemetry reporting to false
+  sed -i '' -E "s/\'default\': true,/\'default\': false,/g" ./src/vs/platform/telemetry/common/telemetryService.ts
+  sed -i '' -E "s/\'default\': true,/\'default\': false,/g" ./src/vs/workbench/services/crashReporter/electron-browser/crashReporterService.ts
 else
   grep -rl --exclude-dir=.git -E $TELEMETRY_URLS . | xargs sed -i -E $REPLACEMENT
+  # set defaults for telemetry reporting to false
+  sed -i -E "s/\'default\': true,/\'default\': false,/g" ./src/vs/platform/telemetry/common/telemetryService.ts
+  sed -i -E "s/\'default\': true,/\'default\': false,/g" ./src/vs/workbench/services/crashReporter/electron-browser/crashReporterService.ts
 fi
 
-# set defaults for telemetry reporting to false
-sed -i "s/\'default\': true,/\'default\': false,/g" ./src/vs/platform/telemetry/common/telemetryService.ts
-sed -i "s/\'default\': true,/\'default\': false,/g" ./src/vs/workbench/services/crashReporter/electron-browser/crashReporterService.ts
