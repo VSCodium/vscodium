6fb85bba3ceb49a2898a59ff6d06bd0e0c9f5b22
Joel Wasserman <joel.wasserman8@gmail.com>
2018-10-18 11:26:39 -0700

    Attempting post yarn replace

--- undo_telemetry.sh
+++ undo_telemetry.sh
@@ -3,9 +3,19 @@
 TELEMETRY_URLS="(dc\.services\.visualstudio\.com)|(vortex\.data\.microsoft\.com)"
 REPLACEMENT="s/$TELEMETRY_URLS/0\.0\.0\.0/g"
 
+TELEMETRY_CRASH_REPORTER="\{\"telemetry.enableCrashReporter\":{type:\"boolean\",description:n.localize(1,null),default:!0"
+TELEMETRY_ENABLE="{\"telemetry.enableTelemetry\":{type:\"boolean\",description:n.localize(1,null),default:!0"
+
+TELEMETRY_CRASH_REPORTER_REPLACEMENT="\{\"telemetry.enableCrashReporter\":{type:\"boolean\",description:n.localize(1,null),default:false"
+TELEMETRY_ENABLE_REPLACEMENT="\{\"telemetry.enableTelemetry\":{type:\"boolean\",description:n.localize(1,null),default:false"
+
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   grep -rl --exclude-dir=.git -E $TELEMETRY_URLS . | xargs sed -i '' -E $REPLACEMENT
+  grep -rl --exclude-dir=.git -E $TELEMETRY_CRASH_REPORTER . | xargs sed -i '' -E $TELEMETRY_CRASH_REPORTER_REPLACEMENT
+  grep -rl --exclude-dir=.git -E $TELEMETRY_ENABLE . | xargs sed -i '' -E $TELEMETRY_ENABLE_REPLACEMENT
 else
   grep -rl --exclude-dir=.git -E $TELEMETRY_URLS . | xargs sed -i -E $REPLACEMENT
+  grep -rl --exclude-dir=.git -E $TELEMETRY_CRASH_REPORTER . | xargs sed -i -E $TELEMETRY_CRASH_REPORTER_REPLACEMENT
+  grep -rl --exclude-dir=.git -E $TELEMETRY_ENABLE . | xargs sed -i -E $TELEMETRY_ENABLE_REPLACEMENT
 fi
 
--- update_settings.sh
+++
@@ -1,8 +0,0 @@
-# set defaults for telemetry reporting to false
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  sed -i '' "s/\'default\': true,/\'default\': false,/g" ./src/vs/platform/telemetry/common/telemetryService.ts
-  sed -i '' "s/\'default\': true,/\'default\': false,/g" ./src/vs/workbench/services/crashReporter/electron-browser/crashReporterService.ts
-else
-  sed -i "s/\'default\': true,/\'default\': false,/g" ./src/vs/platform/telemetry/common/telemetryService.ts
-  sed -i "s/\'default\': true,/\'default\': false,/g" ./src/vs/workbench/services/crashReporter/electron-browser/crashReporterService.ts
-fi
