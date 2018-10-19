247151f03dcff4908b49caa50b76b179674ceb91
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-10-19 06:40:04 -0400

    Whoops

--- update_settings.sh
+++ update_settings.sh
@@ -5,9 +5,9 @@ TELEMETRY_CRASH_REPORTER_REPLACEMENT="s/$TELEMETRY_CRASH_REPORTER/{\"telemetry.e
 TELEMETRY_ENABLE_REPLACEMENT="s/$TELEMETRY_ENABLE/{\"telemetry.enableTelemetry\":{type:\"boolean\",description:n.localize\(1,null\),default:0/g"
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  grep -rl --exclude-dir=.git -E $TELEMETRY_CRASH_REPORTER ../ | xargs sed -i '' -E $TELEMETRY_CRASH_REPORTER_REPLACEMENT
-  grep -rl --exclude-dir=.git -E $TELEMETRY_ENABLE ../ | xargs sed -i '' -E $TELEMETRY_ENABLE_REPLACEMENT
+  grep -rl --exclude-dir=.git -E $TELEMETRY_CRASH_REPORTER . | xargs sed -i '' -E $TELEMETRY_CRASH_REPORTER_REPLACEMENT
+  grep -rl --exclude-dir=.git -E $TELEMETRY_ENABLE . | xargs sed -i '' -E $TELEMETRY_ENABLE_REPLACEMENT
 else
-  grep -rl --exclude-dir=.git -E $TELEMETRY_CRASH_REPORTER ../ | xargs sed -i -E $TELEMETRY_CRASH_REPORTER_REPLACEMENT
-  grep -rl --exclude-dir=.git -E $TELEMETRY_ENABLE ../ | xargs sed -i -E $TELEMETRY_ENABLE_REPLACEMENT
+  grep -rl --exclude-dir=.git -E $TELEMETRY_CRASH_REPORTER . | xargs sed -i -E $TELEMETRY_CRASH_REPORTER_REPLACEMENT
+  grep -rl --exclude-dir=.git -E $TELEMETRY_ENABLE . | xargs sed -i -E $TELEMETRY_ENABLE_REPLACEMENT
 fi
