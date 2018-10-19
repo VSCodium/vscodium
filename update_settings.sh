a15e6fda85b51da3583f6d2aa9a38d3de217a215
Joel Wasserman <joel.wasserman8@gmail.com>
2018-10-19 07:54:38 -0700

    another attempt

--- update_settings.sh
+++ update_settings.sh
@@ -1,17 +1,9 @@
-# TELEMETRY_CRASH_REPORTER="{\"telemetry.enableCrashReporter\":{type:\"boolean\",description:n.localize\(1,null\),default:\!0"
-# TELEMETRY_ENABLE="{\"telemetry.enableTelemetry\":{type:\"boolean\",description:n.localize\(1,null\),default:\!0"
+REPLACEMENT="s/'default': true/'default': false/"
 
-# TELEMETRY_CRASH_REPORTER_REPLACEMENT="s/$TELEMETRY_CRASH_REPORTER/{\"telemetry.enableCrashReporter\":{type:\"boolean\",description:n.localize\(1,null\),default:0/g"
-# TELEMETRY_ENABLE_REPLACEMENT="s/$TELEMETRY_ENABLE/{\"telemetry.enableTelemetry\":{type:\"boolean\",description:n.localize\(1,null\),default:0/g"
-
-# if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-#   grep -rl --exclude-dir=.git -E $TELEMETRY_CRASH_REPORTER ../ | xargs sed -i '' -E $TELEMETRY_CRASH_REPORTER_REPLACEMENT
-#   grep -rl --exclude-dir=.git -E $TELEMETRY_ENABLE ../ | xargs sed -i '' -E $TELEMETRY_ENABLE_REPLACEMENT
-# else
-#   grep -rl --exclude-dir=.git -E $TELEMETRY_CRASH_REPORTER ../ | xargs sed -i -E $TELEMETRY_CRASH_REPORTER_REPLACEMENT
-#   grep -rl --exclude-dir=.git -E $TELEMETRY_ENABLE ../ | xargs sed -i -E $TELEMETRY_ENABLE_REPLACEMENT
-# fi
-
-# set defaults for telemetry reporting to false
-sed -i "s/\'default\': true,/\'default\': false,/g" ./src/vs/platform/telemetry/common/telemetryService.ts
-sed -i "s/\'default\': true,/\'default\': false,/g" ./src/vs/workbench/services/crashReporter/electron-browser/crashReporterService.ts
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  sed -i '' -E "$REPLACEMENT" src/vs/platform/telemetry/common/telemetryService.ts
+  sed -i '' -E "$REPLACEMENT" src/vs/workbench/services/crashReporter/electron-browser/crashReporterService.ts
+else
+  sed -i -E "$REPLACEMENT" src/vs/platform/telemetry/common/telemetryService.ts
+  sed -i -E "$REPLACEMENT" src/vs/workbench/services/crashReporter/electron-browser/crashReporterService.ts
+fi
