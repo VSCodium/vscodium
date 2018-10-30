213baf865109eec82893f46c1ea1ed7f4c6ae146
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-10-30 10:54:21 -0400

    Add -X -y to zip command

--- create_zip.sh
+++ create_zip.sh
@@ -3,7 +3,7 @@
 if [[ "$SHOULD_BUILD" == "yes" ]]; then
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     cd VSCode-darwin
-    zip -r ../VSCode-darwin-${LATEST_MS_TAG}.zip ./*
+    zip -r -X -y ../VSCode-darwin-${LATEST_MS_TAG}.zip ./*
   elif [[ "$BUILDARCH" == "ia32" ]]; then
     cd VSCode-linux-ia32
     tar czf ../VSCode-linux-ia32-${LATEST_MS_TAG}.tar.gz .
