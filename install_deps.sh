ac01cef3987fb879d0b7bfaf565cc12ae39375b6
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-11-13 10:48:10 -0500

    Fix yarn to use latest version on linux installs

--- install_deps.sh
+++ install_deps.sh
@@ -5,7 +5,15 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew install yarn --without-node
   brew install jq zip
 else
+  # handle yarn install
   sudo apt-get update
+  sudo apt-get install apt-transport-https -y --force-yes
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  sudo apt-get update
+  sudo apt-get install --no-install-recommends yarn
+
+  # get other deps
   sudo apt-get install libx11-dev libxkbfile-dev libsecret-1-dev fakeroot rpm jq
   if [[ "$BUILDARCH" == "ia32" ]]; then
     sudo dpkg --add-architecture i386
