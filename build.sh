9b1be50d3f2f123484aae7fc7d5162c8908a0864
Pavan Agrawal <pavanagrawal@outlook.com>
2018-11-20 22:54:17 -0600

    Set up CI with Azure Pipelines change/try
    syntax correction
    
    more updates in pipline steps
    
    mas changes
    
    debug
    
    moar debug
    
    does this be work
    
    another change
    
    have a token
    
    maybe this will work
    
    go forth and build pipleine!
    
    upgrade dat yarn
    
    can yarn self-upgrade?
    
    ?
    
    trial
    
    ?
    
    change yarn
    
    change to Yarn again
    
    python ver change
    
    change script to correctly run windows only
    
    updated build script
    
    working build script?

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "hello"
 if [[ "$SHOULD_BUILD" == "yes" ]]; then
   cp -rp src/* vscode/
   cd vscode
@@ -7,7 +7,6 @@ if [[ "$SHOULD_BUILD" == "yes" ]]; then
   if [[ "$BUILDARCH" == "ia32" ]]; then
     export npm_config_arch=ia32
   fi
-
   ../update_settings.sh
 
   yarn
@@ -18,28 +17,37 @@ if [[ "$SHOULD_BUILD" == "yes" ]]; then
 
   export NODE_ENV=production
 
-  if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
+  if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     # microsoft adds their apt repo to sources
     # unless the app name is code-oss
     # as we are renaming the application to vscodium
     # we need to edit a line in the post install template
     sed -i "s/code-oss/vscodium/" resources/linux/debian/postinst.template
-  fi
+    fi
 
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     npm run gulp vscode-darwin-min
   elif [[ "$BUILDARCH" == "ia32" ]]; then
-    npm run gulp vscode-linux-ia32-min
-    npm run gulp vscode-linux-ia32-build-deb
-    npm run gulp vscode-linux-ia32-build-rpm
-    unset npm_config_arch
+      npm run gulp vscode-linux-ia32-min
+      npm run gulp vscode-linux-ia32-build-deb
+      npm run gulp vscode-linux-ia32-build-rpm
+      unset npm_config_arch
   elif [[ "$BUILDARCH" == "arm64" ]]; then
     npm run gulp vscode-linux-arm64-min
     npm run gulp vscode-linux-arm64-build-deb
     # npm run gulp vscode-linux-arm64-build-rpm
+  elif [[ "$CI_WINDOWS" == "True" ]]; then
+    npx gulp vscode-win32-x64-min
+    npx gulp vscode-win32-x64-copy-inno-updater
+    npx gulp vscode-win32-x64-inno-updater
+    npx gulp vscode-win32-x64-system-setup
+    npx gulp vscode-win32-x64-user-setup
+    npx gulp vscode-win32-x64-archive
   else
     npm run gulp vscode-linux-x64-min
     npm run gulp vscode-linux-x64-build-deb
     npm run gulp vscode-linux-x64-build-rpm
-  fi
+  fi  
+
+  cd ..
 fi
