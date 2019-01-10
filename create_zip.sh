cf28585423c2c73a596237e0356d8a5b468faf51
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-01-10 14:55:28 -0500

    Use VSCodium in name of zip/tar

--- create_zip.sh
+++ create_zip.sh
@@ -3,16 +3,16 @@
 if [[ "$SHOULD_BUILD" == "yes" ]]; then
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     cd VSCode-darwin
-    zip -r -X -y ../VSCode-darwin-${LATEST_MS_TAG}.zip ./*
+    zip -r -X -y ../VSCodium-darwin-${LATEST_MS_TAG}.zip ./*
   elif [[ "$BUILDARCH" == "ia32" ]]; then
     cd VSCode-linux-ia32
-    tar czf ../VSCode-linux-ia32-${LATEST_MS_TAG}.tar.gz .
+    tar czf ../VSCodium-linux-ia32-${LATEST_MS_TAG}.tar.gz .
   elif [[ "$BUILDARCH" == "arm64" ]]; then
     cd VSCode-linux-arm64
-    tar czf ../VSCode-linux-arm64-${LATEST_MS_TAG}.tar.gz .
+    tar czf ../VSCodium-linux-arm64-${LATEST_MS_TAG}.tar.gz .
   else
     cd VSCode-linux-x64
-    tar czf ../VSCode-linux-x64-${LATEST_MS_TAG}.tar.gz .
+    tar czf ../VSCodium-linux-x64-${LATEST_MS_TAG}.tar.gz .
   fi
 
   cd ..
