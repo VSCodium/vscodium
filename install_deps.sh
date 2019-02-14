cffe6216f76a74b5a1b3c4534da657ad55ed113c
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-02-14 10:50:20 -0500

    Correct Linux 32-bit dependency installation

--- install_deps.sh
+++ install_deps.sh
@@ -4,17 +4,20 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew update
   brew install jq zip
 else
-  sudo apt-get update
-  sudo apt-get install libx11-dev libxkbfile-dev libsecret-1-dev fakeroot rpm jq
   if [[ "$BUILDARCH" == "ia32" ]]; then
     sudo dpkg --add-architecture i386
     sudo apt-get update
-    sudo apt-get install libc6-dev-i386 gcc-multilib g++-multilib
-    sudo apt-get install libx11-dev:i386 libxkbfile-dev:i386
+    sudo apt-get install -y fakeroot rpm jq
+    sudo apt-get install -y libx11-dev:i386 libxkbfile-dev:i386 libsecret-1-dev:i386 libc6-dev-i386 gcc-multilib g++-multilib
+    export CFLAGS=-m32
+    export CXXFLAGS=-m32
   elif [[ $BUILDARCH == "arm64" ]]; then
     # Use the default C / C++ compilers,
     # because some makefiles default to CC:=gcc:
     export CC=/usr/bin/cc
     export CXX=/usr/bin/c++
+  else
+    sudo apt-get update
+    sudo apt-get install libx11-dev libxkbfile-dev libsecret-1-dev fakeroot rpm jq
   fi
 fi
