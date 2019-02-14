5e21eb61e74191f29f1b7a3e5451040edbebda1b
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-02-14 14:31:18 -0500

    Add pkg config path

--- install_deps.sh
+++ install_deps.sh
@@ -11,6 +11,7 @@ else
     sudo apt-get install -y libx11-dev:i386 libxkbfile-dev:i386 libsecret-1-dev:i386 libc6-dev-i386 gcc-multilib g++-multilib
     export CFLAGS=-m32
     export CXXFLAGS=-m32
+    export PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig
   elif [[ $BUILDARCH == "arm64" ]]; then
     # Use the default C / C++ compilers,
     # because some makefiles default to CC:=gcc:
