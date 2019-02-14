26adf314b0136c83bc74d757f8eb18b2a2d03ed2
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-02-14 17:24:46 -0500

    Reorder things

--- install_deps.sh
+++ install_deps.sh
@@ -4,12 +4,16 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew update
   brew install jq zip
 else
+  sudo apt-get update
+  sudo apt-get install -y fakeroot rpm jq
   if [[ "$BUILDARCH" == "ia32" ]]; then
     sudo dpkg --add-architecture i386
     sudo apt-get update
-    sudo apt-get upgrade
-    sudo apt-get install -y fakeroot rpm jq
-    sudo apt-get install -y gcc-multilib g++-multilib libglib2.0-dev:i386 libx11-dev:i386 libxkbfile-dev:i386 libsecret-1-dev:i386
+    sudo apt-get install -y gcc-multilib g++-multilib
+    sudo apt-get install -y \
+      libx11-dev:i386 \
+      libxkbfile-dev:i386 \
+      libsecret-1-dev:i386
     export CFLAGS=-m32
     export CXXFLAGS=-m32
     export PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig
@@ -19,7 +23,6 @@ else
     export CC=/usr/bin/cc
     export CXX=/usr/bin/c++
   else
-    sudo apt-get update
     sudo apt-get install libx11-dev libxkbfile-dev libsecret-1-dev fakeroot rpm jq
   fi
 fi
