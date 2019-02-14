cb7b7874b35184485d6da95247b6cac77d299e64
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-02-14 17:09:44 -0500

    Try upgrading

--- install_deps.sh
+++ install_deps.sh
@@ -7,12 +7,9 @@ else
   if [[ "$BUILDARCH" == "ia32" ]]; then
     sudo dpkg --add-architecture i386
     sudo apt-get update
+    sudo apt-get upgrade
     sudo apt-get install -y fakeroot rpm jq
-    sudo apt-get install -y gcc-multilib g++-multilib
-    sudo apt-get install -y libgirepository-1.0-1:i386
-    sudo apt-get install -y gir1.2-glib-2.0:i386
-    sudo apt-get install -y gir1.2-secret-1:i386
-    sudo apt-get install -y libglib2.0-dev:i386 libx11-dev:i386 libxkbfile-dev:i386 libsecret-1-dev:i386
+    sudo apt-get install -y gcc-multilib g++-multilib libglib2.0-dev:i386 libx11-dev:i386 libxkbfile-dev:i386 libsecret-1-dev:i386
     export CFLAGS=-m32
     export CXXFLAGS=-m32
     export PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig
