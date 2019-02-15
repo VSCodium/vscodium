c38cc161c046e293cca0bb0bc4db8ea2a37aec56
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-02-14 19:40:11 -0500

    Fix linux 32 bit builds (#85)
    * Correct Linux 32-bit dependency installation

--- install_deps.sh
+++ install_deps.sh
@@ -5,16 +5,28 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew install jq zip
 else
   sudo apt-get update
-  sudo apt-get install libx11-dev libxkbfile-dev libsecret-1-dev fakeroot rpm jq
+  sudo apt-get install -y fakeroot rpm jq
   if [[ "$BUILDARCH" == "ia32" ]]; then
     sudo dpkg --add-architecture i386
     sudo apt-get update
-    sudo apt-get install libc6-dev-i386 gcc-multilib g++-multilib
-    sudo apt-get install libx11-dev:i386 libxkbfile-dev:i386
+    sudo apt-get install -y gcc-multilib g++-multilib
+    sudo apt-get install -y \
+      libgirepository-1.0-1:i386 \
+      gir1.2-glib-2.0:i386 \
+      libglib2.0-dev:i386 \
+      gir1.2-secret-1:i386 \
+      libx11-dev:i386 \
+      libxkbfile-dev:i386 \
+      libsecret-1-dev:i386
+    export CFLAGS=-m32
+    export CXXFLAGS=-m32
+    export PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig
   elif [[ $BUILDARCH == "arm64" ]]; then
     # Use the default C / C++ compilers,
     # because some makefiles default to CC:=gcc:
     export CC=/usr/bin/cc
     export CXX=/usr/bin/c++
+  else
+    sudo apt-get install libx11-dev libxkbfile-dev libsecret-1-dev fakeroot rpm jq
   fi
 fi
