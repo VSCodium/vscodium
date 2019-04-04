ad0292a17d06374862e1e770fc9ab87d1b397fde
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-04-04 11:19:37 -0700

    Safety logic

--- check_tags.ps1
+++ check_tags.ps1
@@ -1,6 +1,5 @@
 echo "tag: $env:LATEST_MS_TAG"
 $REPO_URI = [uri]$env:BUILD_REPOSITORY_URI
-$REPO_URI
 $USER_REPO = $REPO_URI."LocalPath"
 echo $USER_REPO
 $GITHUB_RESPONSE = curl.exe -s -H "Authorization: token $env:MAPPED_GITHUB_TOKEN" "https://api.github.com/repos/$USER_REPO/releases/tags/$env:LATEST_MS_TAG"
@@ -18,21 +17,22 @@ if (!$env:MAPPED_GITHUB_TOKEN -or $env:MAPPED_GITHUB_TOKEN -like "*GITHUB_TOKEN*
 if (!$VSCODIUM_ASSETS) {
   echo "Release assets do not exist at all, continuing build"
   $SHOULD_BUILD = 'yes'
+} else {
+  $WindowsAssets = ($VSCODIUM_ASSETS | ConvertFrom-Json) | Where-Object { $_.name.Contains('exe') }
+  $SYSTEM_SETUP = $WindowsAssets | Where-Object { $_.name.Contains('Setup') }
+  $USER_SETUP = $WindowsAssets | Where-Object { $_.name.Contains('User') }
+  if (!$SYSTEM_SETUP) {
+    echo "Building on Windows because we have no system-setup.exe";
+    $SHOULD_BUILD = 'yes'
+  }
+  elseif (!$USER_SETUP) {
+    echo "Building on Windows because we have no user-setup.exe";
+    $SHOULD_BUILD = 'yes'
+  }
+  else {
+    echo "Already have all the Windows builds"
+  } 
 }
 
-$WindowsAssets = ($VSCODIUM_ASSETS | ConvertFrom-Json) | Where-Object { $_.name.Contains('exe') }
-$SYSTEM_SETUP = $WindowsAssets | Where-Object { $_.name.Contains('Setup') }
-$USER_SETUP = $WindowsAssets | Where-Object { $_.name.Contains('User') }
-if (!$SYSTEM_SETUP) {
-  echo "Building on Windows because we have no system-setup.exe";
-  $SHOULD_BUILD = 'yes'
-}
-elseif (!$USER_SETUP) {
-  echo "Building on Windows because we have no user-setup.exe";
-  $SHOULD_BUILD = 'yes'
-}
-else {
-  echo "Already have all the Windows builds"
-}
 
 Write-Host "##vso[task.setvariable variable=SHOULD_BUILD]$SHOULD_BUILD"
