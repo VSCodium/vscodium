e54ea7849657c93f229caf2f5bc3e84d5c3c98bc
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-04-04 11:10:58 -0700

    Try again

--- check_tags.ps1
+++ check_tags.ps1
@@ -1,6 +1,8 @@
 echo "tag: $env:LATEST_MS_TAG"
 $REPO_URI = [uri]$env:BUILD_REPOSITORY_URI
-$USER_REPO = $REPO_URI.LocalPath
+$REPO_URI
+$USER_REPO = $REPO_URI."LocalPath"
+echo $USER_REPO
 $GITHUB_RESPONSE = curl.exe -s -H "Authorization: token $env:MAPPED_GITHUB_TOKEN" "https://api.github.com/repos/$USER_REPO/releases/tags/$env:LATEST_MS_TAG"
 echo "Github response: ${GITHUB_RESPONSE}"
 $VSCODIUM_ASSETS= $GITHUB_RESPONSE | jq '.assets'
