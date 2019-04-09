5918cb04a941422c944c0ba41f601d2396fa4136
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-04-09 14:30:51 -0700

    Install dep

--- create_appimage.sh
+++ create_appimage.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 if [[ "$BUILDARCH" == "x64" ]]; then
+  # install a dep needed for this process
+  sudo apt-get install desktop-file-utils
+
   # download pkg2appimage from github
   curl -LO "https://github.com/AppImage/pkg2appimage/raw/master/pkg2appimage"
   
