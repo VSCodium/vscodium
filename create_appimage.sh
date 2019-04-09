c011a632b965c179e0ecd52e0180f72bf39b27c0
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-04-09 12:20:17 -0700

    Update paths

--- create_appimage.sh
+++ create_appimage.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 if [[ "$BUILDARCH" == "x64" ]]; then
-  mkdir appimage
-  cd appimage
-
   # download pkg2appimage from github
   curl -LO "https://github.com/AppImage/pkg2appimage/raw/master/pkg2appimage"
   
-  bash -ex pkg2appimage ../../VSCodium-AppImage-Recipe.yml
-  cd ..
+  bash -e pkg2appimage ../../VSCodium-AppImage-Recipe.yml
 fi
