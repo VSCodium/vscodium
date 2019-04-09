4308942d64c723322e9936997a6ee25809810187
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-04-09 12:58:10 -0700

    Fix yml path

--- create_appimage.sh
+++ create_appimage.sh
@@ -3,5 +3,5 @@ if [[ "$BUILDARCH" == "x64" ]]; then
   # download pkg2appimage from github
   curl -LO "https://github.com/AppImage/pkg2appimage/raw/master/pkg2appimage"
   
-  bash -e pkg2appimage ../../VSCodium-AppImage-Recipe.yml
+  bash -e pkg2appimage ../VSCodium-AppImage-Recipe.yml
 fi
