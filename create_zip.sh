0608a0e812155c63051a07d9fc1489c71273b0ec
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-04-23 17:14:24 -0700

    Sign mac app (#136)

--- create_zip.sh
+++ create_zip.sh
@@ -3,7 +3,7 @@
 if [[ "$SHOULD_BUILD" == "yes" ]]; then
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     cd VSCode-darwin
-    zip -r -X -y ../VSCodium-darwin-${LATEST_MS_TAG}.zip ./*
+    zip -r -X -y ../VSCodium-darwin-${LATEST_MS_TAG}.zip ./*.app
   elif [[ "$BUILDARCH" == "ia32" ]]; then
     cd VSCode-linux-ia32
     tar czf ../VSCodium-linux-ia32-${LATEST_MS_TAG}.tar.gz .
