49d835e783fc00c25abe6244f3e071b072d85722
Randal Stevens <randy@bloombox.io>
2019-05-17 00:42:11 -0700

    Fix target

--- create_dmg.sh
+++ create_dmg.sh
@@ -2,7 +2,7 @@
 if [[ "$SHOULD_BUILD" == "yes" ]]; then
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     cd VSCode-darwin
-    create-dmg VSCodium-darwin-${LATEST_MS_TAG}.app ../
+    create-dmg VSCodium-darwin-${LATEST_MS_TAG}.app ..
   fi
   cd ..
 fi
