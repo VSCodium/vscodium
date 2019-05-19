798eb8cb11650171f6cd4675263e9bbd1deefdbc
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-05-18 19:15:47 -0700

    Use checksum for Windows builds

--- sum.sh
+++ sum.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# shasum blows up in Azure, so using this
+# node package which has similar syntax and identical output
+if [[ "$CI_WINDOWS" == "True" ]]; then
+  npm i -g checksum
+fi
+
 sum_file () {
   if [[ -f "$1" ]]; then
-    shasum -a 256 $1 > $1.sha256
-    shasum $1 > $1.sha1
+    if [[ "$CI_WINDOWS" == "True" ]]; then
+      checksum -a sha256 $1 > $1.sha256
+      checksum -a sha1 $1 > $1.sha1
+    else
+      shasum -a 256 $1 > $1.sha256
+      shasum $1 > $1.sha1
+    fi
   fi
 }
 
