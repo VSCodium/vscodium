cd6e1485b5c0fedbc60ca3ba8e5791946944fe8d
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-09-04 15:39:23 -0700

    Stop triggering Azure

--- trigger-azure.sh
+++
@@ -1,7 +0,0 @@
-if [ "$AZURE_TOKEN" != "" ]; then
-  if [[ "$SHOULD_BUILD" == "yes" ]]; then
-    if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-      curl -X POST -H "Content-Type: application/json" -H "Authorization: Basic $AZURE_TOKEN" -d '{"definition":{"id":1}}' https://dev.azure.com/VSCodium/vscodium/_apis/build/builds?api-version=5.0-preview.5
-    fi
-  fi
-fi
