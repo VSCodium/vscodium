60335dcd6feac55d1092baf056a22982090171b5
Brian <ting779@gmail.com>
2019-11-05 08:25:16 +0800

    Update create_appimage.sh
    Addresses issue #253 
    Modified pkg2appimage removes PYTHONPATH & PYTHONHOME exportations.

--- create_appimage.sh
+++ create_appimage.sh
@@ -3,8 +3,8 @@ if [[ "$BUILDARCH" == "x64" ]]; then
   # install a dep needed for this process
   sudo apt-get install desktop-file-utils
 
-  # download pkg2appimage from github
-  curl -LO "https://github.com/AppImage/pkg2appimage/raw/master/pkg2appimage"
+  # download modified pkg2appimage from gist
+  curl -LO "https://gist.githubusercontent.com/tyu1996/39b8a7a29c2cdeef882daaa1702ec971/raw/2e788010deafb2f1214439ee0bc8e46685480e45/pkg2appimage"
   
   bash -e pkg2appimage ../VSCodium-AppImage-Recipe.yml
 fi
