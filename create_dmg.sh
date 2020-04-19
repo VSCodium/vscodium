0ca513930d1248a5de1a46cf2becd6740bf4b948
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-04-19 12:50:45 -0700

    Remove space from dmg filename

--- create_dmg.sh
+++ create_dmg.sh
@@ -3,6 +3,7 @@ if [[ "$SHOULD_BUILD" == "yes" ]]; then
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     cd VSCode-darwin
     create-dmg VSCodium.app ..
+    mv "../VSCodium ${LATEST_MS_TAG}.dmg" "../VSCodium.${LATEST_MS_TAG}.dmg"
   fi
   cd ..
 fi
