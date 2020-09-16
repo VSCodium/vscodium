99038981dc11ebe58155570fd001013df539421d
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-09-16 11:21:30 -0700

    (experiment) remove jq double quotes

--- get_repo.sh
+++ get_repo.sh
@@ -2,8 +2,8 @@
 
 # figure out latest tag by calling MS update API
 UPDATE_INFO=$(curl https://update.code.visualstudio.com/api/update/darwin/stable/lol)
-export LATEST_MS_COMMIT=$(echo $UPDATE_INFO | jq '.version')
-export LATEST_MS_TAG=$(echo $UPDATE_INFO | jq '.name')
+export LATEST_MS_COMMIT=$(echo $UPDATE_INFO | jq -r '.version')
+export LATEST_MS_TAG=$(echo $UPDATE_INFO | jq -r '.name')
 echo "Got the latest MS tag: ${LATEST_MS_TAG}"
 
 git clone https://github.com/Microsoft/vscode.git --branch $LATEST_MS_TAG --depth 1
