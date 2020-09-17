101bda7072bfda6d8d7302a215459f6b62668b82
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-09-17 15:30:09 -0700

    (experiment) disable changes

--- create_dmg.sh
+++ create_dmg.sh
@@ -4,7 +4,7 @@ set -x
 if [[ "$SHOULD_BUILD" == "yes" ]]; then
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     cd VSCode-darwin
-    create-dmg VSCodium.app ..
+    npx create-dmg VSCodium.app ..
     mv "../VSCodium ${LATEST_MS_TAG}.dmg" "../VSCodium.${LATEST_MS_TAG}.dmg"
   fi
   cd ..
