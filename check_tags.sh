9a046ea3a0cb4b27aa3daa473a0d3c80c5888874
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-09-18 05:57:00 -0700

    (experiment) set repository according to GH action env var

--- check_tags.sh
+++ check_tags.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-REPOSITORY=${TRAVIS_REPO_SLUG:-"VSCodium/vscodium"}
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  REPOSITORY=$GITHUB_REPOSITORY
+else
+  REPOSITORY=${TRAVIS_REPO_SLUG:-"VSCodium/vscodium"}
+fi
 GITHUB_RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$REPOSITORY/releases/tags/$LATEST_MS_TAG)
 echo "Github response: ${GITHUB_RESPONSE}"
 VSCODIUM_ASSETS=$(echo $GITHUB_RESPONSE | jq '.assets')
