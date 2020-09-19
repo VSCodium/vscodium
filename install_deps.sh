a8d355c27624cc3d434c135a5f28eaf889effb58
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-09-18 17:02:52 -0700

    (experiment) create zip for Linux and remove osx deps from travis

--- install_deps.sh
+++ install_deps.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  curl -Lo $HOME/bin/jq --create-dirs https://github.com/stedolan/jq/releases/download/jq-1.6/jq-osx-amd64
-  chmod +x $HOME/bin/jq
-  export PATH="$HOME/bin:$PATH"
-else
-  sudo apt-get update
-  sudo apt-get install -y fakeroot jq
-  triplet=
-  case $BUILDARCH in
+sudo apt-get update
+sudo apt-get install -y fakeroot jq
+triplet=
+case $BUILDARCH in
   arm)
     arch=armhf
     triplet=arm-linux-gnueabihf
@@ -18,25 +13,24 @@ else
     arch=arm64
     triplet=aarch64-linux-gnu
     ;;
-  esac
+esac
 
-  if [[ -n "$triplet" ]]; then
-    sed 's/^deb /deb [arch=amd64] '/g -i /etc/apt/sources.list
-    echo "deb [arch=$arch] http://ports.ubuntu.com/ubuntu-ports/ trusty main" | sudo tee -a /etc/apt/sources.list.d/$arch.list >/dev/null
-    sudo dpkg --add-architecture $arch
-    sudo apt-get update
-    sudo apt-get install libc6-dev-$arch-cross gcc-$triplet g++-$triplet `apt-cache search x11proto | grep ^x11proto | cut -f 1 -d ' '` xz-utils pkg-config
-    mkdir -p dl
-    cd dl
-    apt-get download libx11-dev:$arch libx11-6:$arch libxkbfile-dev:$arch libxkbfile1:$arch libxau-dev:$arch libxdmcp-dev:$arch libxcb1-dev:$arch libsecret-1-dev:$arch libsecret-1-0:$arch libpthread-stubs0-dev:$arch libglib2.0-dev:$arch libglib2.0-0:$arch libffi-dev:$arch libffi6:$arch zlib1g:$arch libpcre3-dev:$arch libpcre3:$arch
-    for i in *.deb; do ar x $i; sudo tar -C / -xf data.tar.*; rm -f data.tar.*; done
-    cd ..
-    export CC=/usr/bin/$triplet-gcc
-    export CXX=/usr/bin/$triplet-g++
-    export CC_host=/usr/bin/gcc
-    export CXX_host=/usr/bin/g++
-    export PKG_CONFIG_LIBDIR=/usr/lib/$triplet/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig
-  else
-    sudo apt-get install libx11-dev libxkbfile-dev libsecret-1-dev rpm
-  fi
+if [[ -n "$triplet" ]]; then
+  sed 's/^deb /deb [arch=amd64] '/g -i /etc/apt/sources.list
+  echo "deb [arch=$arch] http://ports.ubuntu.com/ubuntu-ports/ trusty main" | sudo tee -a /etc/apt/sources.list.d/$arch.list >/dev/null
+  sudo dpkg --add-architecture $arch
+  sudo apt-get update
+  sudo apt-get install libc6-dev-$arch-cross gcc-$triplet g++-$triplet `apt-cache search x11proto | grep ^x11proto | cut -f 1 -d ' '` xz-utils pkg-config
+  mkdir -p dl
+  cd dl
+  apt-get download libx11-dev:$arch libx11-6:$arch libxkbfile-dev:$arch libxkbfile1:$arch libxau-dev:$arch libxdmcp-dev:$arch libxcb1-dev:$arch libsecret-1-dev:$arch libsecret-1-0:$arch libpthread-stubs0-dev:$arch libglib2.0-dev:$arch libglib2.0-0:$arch libffi-dev:$arch libffi6:$arch zlib1g:$arch libpcre3-dev:$arch libpcre3:$arch
+  for i in *.deb; do ar x $i; sudo tar -C / -xf data.tar.*; rm -f data.tar.*; done
+  cd ..
+  export CC=/usr/bin/$triplet-gcc
+  export CXX=/usr/bin/$triplet-g++
+  export CC_host=/usr/bin/gcc
+  export CXX_host=/usr/bin/g++
+  export PKG_CONFIG_LIBDIR=/usr/lib/$triplet/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig
+else
+  sudo apt-get install libx11-dev libxkbfile-dev libsecret-1-dev rpm
 fi
