3c5eb104f9421c8f48c4884d5f5d8ec53ab7ac98
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-10-08 21:29:50 -0700

    Disable AppImage creation since it isn't working in Docker container

--- create_appimage.sh
+++ create_appimage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [[ "$VSCODE_ARCH" == "x64" ]]; then
-  # install a dep needed for this process
-  sudo apt-get install desktop-file-utils
-
-  cd ..
-  
-  bash -e src/resources/linux/appimage/pkg2appimage VSCodium-AppImage-Recipe.yml
-fi
+# if [[ "$VSCODE_ARCH" == "x64" ]]; then
+#   # install a dep needed for this process
+#   sudo apt-get install desktop-file-utils
+# 
+#   cd ..
+#   
+#   bash -e src/resources/linux/appimage/pkg2appimage VSCodium-AppImage-Recipe.yml
+# fi
