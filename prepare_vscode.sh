23bd025392290ea100ca57da645f9a9a7b4d1bf6
articlecat <38485721+articlecat@users.noreply.github.com>
2020-10-19 06:01:04 -0700

    Apply custom-extensions-gallery.patch in prepare_vscode.sh

--- prepare_vscode.sh
+++ prepare_vscode.sh
@@ -13,6 +13,7 @@ cd vscode || exit
 ../update_settings.sh
 
 # apply patches
+patch -u src/vs/platform/product/common/product.ts -i ../patches/custom-extensions-gallery.patch
 patch -u src/vs/platform/update/electron-main/updateService.win32.ts -i ../patches/update-cache-path.patch
 
 if [[ "$OS_NAME" == "osx" ]]; then
