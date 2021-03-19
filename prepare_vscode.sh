74588ec2a806eb1d8e0234bc12d82c74c409dae0
Baptiste Augrain <daiyam@zokugun.org>
2021-03-19 04:47:31 +0100

    add patch to use github personal access token instead of auth relay server

--- prepare_vscode.sh
+++ prepare_vscode.sh
@@ -10,6 +10,7 @@ cd vscode || exit
 # apply patches
 patch -u src/vs/platform/update/electron-main/updateService.win32.ts -i ../patches/update-cache-path.patch
 patch -u resources/linux/rpm/code.spec.template -i ../patches/fix-rpm-spec.patch
+patch -u extensions/github-authentication/src/githubServer.ts -i ../patches/use-github-pat.patch
 
 if [[ "$OS_NAME" == "osx" ]]; then
   CHILD_CONCURRENCY=1 yarn --frozen-lockfile --ignore-optional
