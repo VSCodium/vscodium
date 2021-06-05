52a0f12ac1f88214abf3bd43dea004b70bf2c30b
Baptiste Augrain <daiyam@zokugun.org>
2021-06-05 12:15:56 +0200

    generate zsync file to make the appimage updateable

--- create_appimage.sh
+++ create_appimage.sh
@@ -8,7 +8,11 @@ if [[ "$VSCODE_ARCH" == "x64" ]]; then
 
   wget -c https://github.com/$(wget -q https://github.com/AppImage/pkg2appimage/releases -O - | grep "pkg2appimage-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)
   chmod +x ./pkg2appimage-*.AppImage
-  ./pkg2appimage-*.AppImage VSCodium-AppImage-Recipe.yml
+  
+  ./pkg2appimage-*.AppImage --appimage-extract
+  sed -i 's/generate_type2_appimage/generate_type2_appimage -u "gh-releases-zsync|VSCodium|vscodium|latest|*.AppImage.zsync"/g' squashfs-root/AppRun
+  
+  squashfs-root/AppRun VSCodium-AppImage-Recipe.yml
 fi
 
 cd vscode
