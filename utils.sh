66551077f83d86d3f200f88c09ebd621d89b8657
swinder0161 <swinder0161@gmail.com>
2021-07-22 02:19:00 +0530

    Add support to use GNU sed installed in mac osx (#770)
    * Add support to use GNU sed installed in mac osx
    
    * Move common functions to utils.sh
    
    Co-authored-by: Swinder Pal Singh Juneja <swinder.juneja@oneplus.com>

---
+++ utils.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#All common functions can be added to this file
+
+is_gnu_sed () {
+  sed --version >/dev/null 2>&1
+}
+
+replace () {
+  echo $1
+  if is_gnu_sed; then
+    sed -i -E "$1" $2
+  else
+    sed -i '' -E "$1" $2
+  fi
+}
