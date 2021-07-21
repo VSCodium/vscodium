797609be42b187da496e6367e632758d708c7157
Swinder Pal Singh Juneja <swinder.juneja@oneplus.com>
2021-07-21 21:48:59 +0530

    Add support to use GNU sed installed in mac osx

--- undo_telemetry.sh
+++ undo_telemetry.sh
@@ -4,7 +4,11 @@ TELEMETRY_URLS="(dc\.services\.visualstudio\.com)|(vortex\.data\.microsoft\.com)
 REPLACEMENT="s/$TELEMETRY_URLS/0\.0\.0\.0/g"
 
 if [[ "$OS_NAME" == "osx" ]]; then
-  grep -rl --exclude-dir=.git -E $TELEMETRY_URLS . | xargs sed -i '' -E $REPLACEMENT
+  if sed --version 2>&1 | grep -q GNU; then
+    grep -rl --exclude-dir=.git -E $TELEMETRY_URLS . | xargs sed -i -E $REPLACEMENT
+  else
+    grep -rl --exclude-dir=.git -E $TELEMETRY_URLS . | xargs sed -i '' -E $REPLACEMENT
+  fi
 else
   grep -rl --exclude-dir=.git -E $TELEMETRY_URLS . | xargs sed -i -E $REPLACEMENT
 fi
