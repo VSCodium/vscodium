a95d10ce754e0f187a84fb0e2c7ea9b5a3749a60
Baptiste Augrain <daiyam@zokugun.org>
2021-09-25 17:46:04 +0200

    enhance: add logs

--- prepare_artifacts.sh
+++ prepare_artifacts.sh
@@ -6,6 +6,7 @@ npm install -g checksum
 
 sum_file() {
   if [[ -f "$1" ]]; then
+    echo "Calcuating checksum for $1"
     checksum -a sha256 "$1" > "$1".sha256
     checksum "$1" > "$1".sha1
   fi
@@ -15,12 +16,14 @@ mkdir artifacts
 
 if [[ "${OS_NAME}" == "osx" ]]; then
   if [[ "${SHOULD_BUILD_ZIP}" != "no" ]]; then
+    echo "Building and moving ZIP"
     cd "VSCode-darwin-${VSCODE_ARCH}"
     zip -r -X -y ../artifacts/VSCodium-darwin-${VSCODE_ARCH}-${MS_TAG}.zip ./*.app
     cd ..
   fi
 
   if [[ "${SHOULD_BUILD_DMG}" != "no" ]]; then
+    echo "Building and moving DMG"
     pushd "VSCode-darwin-${VSCODE_ARCH}"
     npx create-dmg VSCodium.app ..
     mv "../VSCodium ${MS_TAG}.dmg" "../artifacts/VSCodium.${VSCODE_ARCH}.${MS_TAG}.dmg"
@@ -28,49 +31,62 @@ if [[ "${OS_NAME}" == "osx" ]]; then
   fi
 elif [[ "${OS_NAME}" == "windows" ]]; then
   if [[ "${SHOULD_BUILD_ZIP}" != "no" ]]; then
+    echo "Moving ZIP"
     mv vscode\\.build\\win32-${VSCODE_ARCH}\\archive\\VSCode-win32-${VSCODE_ARCH}.zip artifacts\\VSCodium-win32-${VSCODE_ARCH}-${MS_TAG}.zip
   fi
 
   if [[ "${SHOULD_BUILD_EXE_SYS}" != "no" ]]; then
+    echo "Moving System EXE"
     mv vscode\\.build\\win32-${VSCODE_ARCH}\\system-setup\\VSCodeSetup.exe artifacts\\VSCodiumSetup-${VSCODE_ARCH}-${MS_TAG}.exe
   fi
 
   if [[ "${SHOULD_BUILD_EXE_USR}" != "no" ]]; then
+    echo "Moving User EXE"
     mv vscode\\.build\\win32-${VSCODE_ARCH}\\user-setup\\VSCodeSetup.exe artifacts\\VSCodiumUserSetup-${VSCODE_ARCH}-${MS_TAG}.exe
   fi
 
   if [[ "${VSCODE_ARCH}" == "ia32" || "${VSCODE_ARCH}" == "x64" ]]; then
     if [[ "${SHOULD_BUILD_MSI}" != "no" ]]; then
+      echo "Moving MSI"
       mv build\\windows\\msi\\releasedir\\VSCodium-${VSCODE_ARCH}-${MS_TAG}.msi artifacts/
     fi
 
     if [[ "${SHOULD_BUILD_MSI_NOUP}" != "no" ]]; then
+      echo "Moving MSI with disabled updates"
       mv build\\windows\\msi\\releasedir\\VSCodium-${VSCODE_ARCH}-updates-disabled-${MS_TAG}.msi artifacts/
     fi
   fi
 else
   if [[ "${SHOULD_BUILD_TAR}" != "no" ]]; then
+    echo "Building and moving TAR"
     cd VSCode-linux-${VSCODE_ARCH}
     tar czf ../artifacts/VSCodium-linux-${VSCODE_ARCH}-${MS_TAG}.tar.gz .
     cd ..
   fi
 
   if [[ "${SHOULD_BUILD_DEB}" != "no" ]]; then
+    echo "Moving DEB"
     mv vscode/.build/linux/deb/*/deb/*.deb artifacts/
   fi
 
   if [[ "${SHOULD_BUILD_RPM}" != "no" ]]; then
+    echo "Moving RPM"
     mv vscode/.build/linux/rpm/*/*.rpm artifacts/
   fi
 
   if [[ "${VSCODE_ARCH}" == "x64" ]] && [["${SHOULD_BUILD_APPIMAGE}" != "no" ]]; then
+    echo "Moving AppImage"
     mv build/linux/appimage/out/*.AppImage* artifacts/
   fi
 fi
 
-for FILE in ./artifacts/*
+cd artifacts
+
+for FILE in *
 do
   if [[ -f "${FILE}" ]]; then
     sum_file "${FILE}"
   fi
 done
+
+cd ..
--- release.sh
+++ release.sh
@@ -10,12 +10,14 @@ fi
 echo "${GH_CLI_TOKEN}" | gh auth login --with-token
 
 if [[ $( gh release view "${MS_TAG}" 2>&1 ) =~ "release not found" ]]; then
+  echo "Creating release `${MS_TAG}`"
   gh release create "${MS_TAG}"
 fi
 
 for FILE in ./artifacts/*
 do
   if [[ -f "${FILE}" ]] && [[ "${FILE}" != *.sha1 ]] && [[ "${FILE}" != *.sha256 ]]; then
+    echo "Uploading `${FILE}`"
     gh release upload "${MS_TAG}" "${FILE}" "${FILE}.sha1" "${FILE}.sha256"
   fi
 done
