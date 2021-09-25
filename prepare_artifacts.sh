2ed9b588d3e7437b606f1bf6de3e3fef96b440d1
Baptiste Augrain <daiyam@zokugun.org>
2021-09-25 16:32:15 +0200

    fix: move artifacts only when needed

--- prepare_artifacts.sh
+++ prepare_artifacts.sh
@@ -14,47 +14,57 @@ sum_file() {
 mkdir artifacts
 
 if [[ "${OS_NAME}" == "osx" ]]; then
-  if [["${SHOULD_BUILD_ZIP}" != "no" ]]; then
+  if [[ "${SHOULD_BUILD_ZIP}" != "no" ]]; then
     cd "VSCode-darwin-${VSCODE_ARCH}"
-
     zip -r -X -y ../artifacts/VSCodium-darwin-${VSCODE_ARCH}-${MS_TAG}.zip ./*.app
-
     cd ..
   fi
 
-  if [["${SHOULD_BUILD_DMG}" != "no" ]]; then
+  if [[ "${SHOULD_BUILD_DMG}" != "no" ]]; then
     pushd "VSCode-darwin-${VSCODE_ARCH}"
     npx create-dmg VSCodium.app ..
     mv "../VSCodium ${MS_TAG}.dmg" "../artifacts/VSCodium.${VSCODE_ARCH}.${MS_TAG}.dmg"
     popd
   fi
 elif [[ "${OS_NAME}" == "windows" ]]; then
-  mv vscode\\.build\\win32-${VSCODE_ARCH}\\system-setup\\VSCodeSetup.exe artifacts\\VSCodiumSetup-${VSCODE_ARCH}-${MS_TAG}.exe
-  mv vscode\\.build\\win32-${VSCODE_ARCH}\\user-setup\\VSCodeSetup.exe artifacts\\VSCodiumUserSetup-${VSCODE_ARCH}-${MS_TAG}.exe
-  mv vscode\\.build\\win32-${VSCODE_ARCH}\\archive\\VSCode-win32-${VSCODE_ARCH}.zip artifacts\\VSCodium-win32-${VSCODE_ARCH}-${MS_TAG}.zip
+  if [[ "${SHOULD_BUILD_ZIP}" != "no" ]]; then
+    mv vscode\\.build\\win32-${VSCODE_ARCH}\\archive\\VSCode-win32-${VSCODE_ARCH}.zip artifacts\\VSCodium-win32-${VSCODE_ARCH}-${MS_TAG}.zip
+  fi
+
+  if [[ "${SHOULD_BUILD_EXE_SYS}" != "no" ]]; then
+    mv vscode\\.build\\win32-${VSCODE_ARCH}\\system-setup\\VSCodeSetup.exe artifacts\\VSCodiumSetup-${VSCODE_ARCH}-${MS_TAG}.exe
+  fi
+
+  if [[ "${SHOULD_BUILD_EXE_USR}" != "no" ]]; then
+    mv vscode\\.build\\win32-${VSCODE_ARCH}\\user-setup\\VSCodeSetup.exe artifacts\\VSCodiumUserSetup-${VSCODE_ARCH}-${MS_TAG}.exe
+  fi
 
   if [[ "${VSCODE_ARCH}" == "ia32" || "${VSCODE_ARCH}" == "x64" ]]; then
-    mv build\\windows\\msi\\releasedir\\VSCodium-${VSCODE_ARCH}-${MS_TAG}.msi artifacts
-    mv build\\windows\\msi\\releasedir\\VSCodium-${VSCODE_ARCH}-updates-disabled-${MS_TAG}.msi artifacts
+    if [[ "${SHOULD_BUILD_MSI}" != "no" ]]; then
+      mv build\\windows\\msi\\releasedir\\VSCodium-${VSCODE_ARCH}-${MS_TAG}.msi artifacts/
+    fi
+
+    if [[ "${SHOULD_BUILD_MSI_NOUP}" != "no" ]]; then
+      mv build\\windows\\msi\\releasedir\\VSCodium-${VSCODE_ARCH}-updates-disabled-${MS_TAG}.msi artifacts/
+    fi
   fi
 else
-  if [["${SHOULD_BUILD_TAR}" != "no" ]]; then
-    echo "Tar release"
-
+  if [[ "${SHOULD_BUILD_TAR}" != "no" ]]; then
     cd VSCode-linux-${VSCODE_ARCH}
-
     tar czf ../artifacts/VSCodium-linux-${VSCODE_ARCH}-${MS_TAG}.tar.gz .
-
     cd ..
   fi
 
-  echo "Move/rename build artifacts"
+  if [[ "${SHOULD_BUILD_DEB}" != "no" ]]; then
+    mv vscode/.build/linux/deb/*/deb/*.deb artifacts/
+  fi
 
-  cp vscode/.build/linux/deb/*/deb/*.deb artifacts/
-  cp vscode/.build/linux/rpm/*/*.rpm artifacts/
+  if [[ "${SHOULD_BUILD_RPM}" != "no" ]]; then
+    mv vscode/.build/linux/rpm/*/*.rpm artifacts/
+  fi
 
-  if [[ "${VSCODE_ARCH}" == "x64" ]]; then
-    cp build/linux/appimage/out/*.AppImage* artifacts/
+  if [[ "${VSCODE_ARCH}" == "x64" ]] && [["${SHOULD_BUILD_APPIMAGE}" != "no" ]]; then
+    mv build/linux/appimage/out/*.AppImage* artifacts/
   fi
 fi
 
