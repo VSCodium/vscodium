36b06dd66b734e87c4d88ec2d976494e77b521a7
Baptiste Augrain <daiyam@zokugun.org>
2021-12-18 00:37:49 +0100

    fix: correct script to get latest snap version (#945)

--- stores/snapcraft/check_deploy.sh
+++ stores/snapcraft/check_deploy.sh
@@ -13,10 +13,8 @@ elif [[ "${GITHUB_EVENT_NAME}" == "push" ]]; then
 else
 	echo "It's a cron"
 
-  sudo apt install curl jq
-
   sudo curl -sS --unix-socket /run/snapd.socket http://localhost/v2/find\?q\=codium > snap_latest.json
-  SNAP_VERSION=$(jq -r '.result|map(select(.id == "lIZWXTqmo6LFSts5Cgk2VPlNwtysZAeH"))|.version' snap_latest.json)
+  SNAP_VERSION=$(jq -r '.result|map(select(.id == "lIZWXTqmo6LFSts5Cgk2VPlNwtysZAeH"))|last.version' snap_latest.json)
   echo "Snap version: ${SNAP_VERSION}"
 
   wget --quiet https://api.github.com/repos/VSCodium/vscodium/releases -O gh_latest.json
