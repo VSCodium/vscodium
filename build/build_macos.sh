5d2a62452acb9d76d245b0389da49929dd1d89db
Baptiste Augrain <daiyam@zokugun.org>
2022-05-16 01:34:17 +0200

    feat(macos): build script is detecting arch

--- build/build_macos.sh
+++ build/build_macos.sh
@@ -3,6 +3,16 @@
 rm -rf VSCode*
 rm -rf vscode
 
+UNAME_ARCH=$( uname -m )
+
+if [[ "${UNAME_ARCH}" == "arm64" ]]; then
+    export VSCODE_ARCH="arm64"
+else
+    export VSCODE_ARCH="x64"
+fi
+
+echo "-- VSCODE_ARCH: ${VSCODE_ARCH}"
+
 . get_repo.sh
 
-SHOULD_BUILD=yes CI_BUILD=no OS_NAME=osx VSCODE_ARCH=x64 . build.sh
+SHOULD_BUILD=yes CI_BUILD=no OS_NAME=osx . build.sh
