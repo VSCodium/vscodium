3fa8eee9feb30c5b39bc53886a6fdabfaca969d7
Baptiste Augrain <daiyam@zokugun.org>
2022-06-20 10:34:50 +0200

    feat: add script to update patches (#1138)

---
+++ build/update_patches.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd vscode || { echo "'vscode' dir not found"; exit 1; }
+
+git add .
+git reset -q --hard HEAD
+
+for file in ../patches/*.patch; do
+  if [ -f "${file}" ]; then
+    echo applying patch: "${file}"
+    git apply --ignore-whitespace "${file}"
+    if [ $? -ne 0 ]; then
+      echo failed to apply patch "${file}"
+      git apply --reject "${file}"
+      read -p "Press any key when the conflict have been resolved..." -n1 -s
+      git diff > "${file}"
+    fi
+    git add .
+    git reset -q --hard HEAD
+  fi
+done
