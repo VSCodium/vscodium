fb5d460358c8dc5aced708b4c28457019ec754b9
Baptiste Augrain <daiyam@zokugun.org>
2022-07-23 00:39:17 +0200

    fix: remove milliseconds timing

--- undo_telemetry.sh
+++ undo_telemetry.sh
@@ -6,30 +6,20 @@ REPLACEMENT="s/${TELEMETRY_URLS}/0\.0\.0\.0/g"
 #include common functions
 . ../utils.sh
 
-debug() {
-  d1=`perl -MTime::HiRes=time -e 'printf "%.5f\n", time'`
+d1=`gdate +%s`
 
-  sed -i -E "${REPLACEMENT}" "${1}"
-
-  d2=`perl -MTime::HiRes=time -e 'printf "%.5f\n", time'`
-
-  echo "found: ${1} ($( du -h ${1} | cut -f1 )) $( echo "(${d2} - ${d1}) * 1000" | bc )ms"
-}
-
-export -f debug
-
-d1=`perl -MTime::HiRes=time -e 'printf "%.5f\n", time'`
-
-if [[ "${OS_NAME}" == "osx" ]]; then
+if [[ "${OS_NAME}" == "linux" ]]; then
+  rg --no-ignore -l "${TELEMETRY_URLS}" . | xargs -I {} bash -c 'echo "found: $@ (`date`)"; sed -i -E "$@"' _ {}
+elif [[ "${OS_NAME}" == "osx" ]]; then
   if is_gnu_sed; then
-    ./node_modules/@vscode/ripgrep/bin/rg --no-ignore -l "${TELEMETRY_URLS}" . | xargs sed -i -E "${REPLACEMENT}"
+    ./node_modules/@vscode/ripgrep/bin/rg --no-ignore -l "${TELEMETRY_URLS}" . | xargs -I {} bash -c 'echo "found: $@ (`date`)"; sed -i -E "$@"' _ {}
   else
-    ./node_modules/@vscode/ripgrep/bin/rg --no-ignore -l "${TELEMETRY_URLS}" . | xargs sed -i '' -E "${REPLACEMENT}"
+    ./node_modules/@vscode/ripgrep/bin/rg --no-ignore -l "${TELEMETRY_URLS}" . | xargs -I {} bash -c 'echo "found: $@ (`date`)"; sed -i \''\'' -E "$@"' _ {}
   fi
 else
-  ./node_modules/@vscode/ripgrep/bin/rg --no-ignore -l "${TELEMETRY_URLS}" . | xargs -I {} bash -c 'debug "$@"' _ {}
+  ./node_modules/@vscode/ripgrep/bin/rg --no-ignore --path-separator=// -l "${TELEMETRY_URLS}" . | xargs -I {} bash -c 'echo "found: $@ (`date`)"; sed -i -E "$@"' _ {}
 fi
 
-d2=`perl -MTime::HiRes=time -e 'printf "%.5f\n", time'`
+d2=`date +%s`
 
-echo "undo_telemetry: $( echo "${d2} - ${d1}" | bc )s"
+echo "undo_telemetry: $( echo "${d2} - ${d1}" )s"
