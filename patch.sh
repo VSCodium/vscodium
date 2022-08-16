a4e5b2a1012e83868598227a57b24411f21eba91
Baptiste Augrain <daiyam@zokugun.org>
2022-08-16 23:37:20 +0200

    fix: update issue reporter (#1206)

---
+++ patch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FILE="../patches/${1}.patch"
+
+cd vscode || { echo "'vscode' dir not found"; exit 1; }
+
+git apply --reject "${FILE}"
+
+read -p "Press any key when the conflict have been resolved..." -n1 -s
+
+git diff -U1 > "${FILE}"
+
+cd ..
+
+echo "The patch has been generated."
