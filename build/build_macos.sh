af83e30053577ad8c17c68d45db16ce908aec7bb
Baptiste Augrain <daiyam@zokugun.org>
2022-08-16 22:37:59 +0200

    doc: update build doc and add script for docker

--- build/build_macos.sh
+++ build/build_macos.sh
@@ -6,9 +6,9 @@ rm -rf vscode*
 UNAME_ARCH=$( uname -m )
 
 if [[ "${UNAME_ARCH}" == "arm64" ]]; then
-    export VSCODE_ARCH="arm64"
+  export VSCODE_ARCH="arm64"
 else
-    export VSCODE_ARCH="x64"
+  export VSCODE_ARCH="x64"
 fi
 
 echo "-- VSCODE_ARCH: ${VSCODE_ARCH}"
