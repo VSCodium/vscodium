b63511cd1755e68c171737e15fdfb047533f43bc
Baptiste Augrain <daiyam@zokugun.org>
2022-08-26 01:35:44 +0200

    feat: add green icon

--- prepare_vscode.sh
+++ prepare_vscode.sh
@@ -5,7 +5,12 @@ set -e
 # include common functions
 . ./utils.sh
 
-cp -rp src/* vscode/
+if [[ "${INSIDER}" == "yes" ]]; then
+  cp -rp src/insider/* vscode/
+else
+  cp -rp src/stable/* vscode/
+fi
+
 cp -f LICENSE vscode/LICENSE.txt
 
 cd vscode || { echo "'vscode' dir not found"; exit 1; }
