4e20dbd99bac8c2d235d18fa1846404b34f78130
Baptiste Augrain <daiyam@zokugun.org>
2022-08-29 23:55:37 +0200

    fix(windows): correctly replace variables [skip ci]

--- build/windows/msi/build.sh
+++ build/windows/msi/build.sh
@@ -12,10 +12,12 @@ WIN_SDK_FULL_VERSION="10.0.17763.0"
 if [[ "${VSCODE_QUALITY}" == "insider" ]]; then
   PRODUCT_NAME="VSCodium - Insiders"
   PRODUCT_SKU="vscodium-insiders"
+  PRODUCT_UPGRADE_CODE="1C9B7195-5A9A-43B3-B4BD-583E20498467"
   ICON_DIR="..\\..\\..\\src\\insider\\resources\\win32"
 else
   PRODUCT_NAME="VSCodium"
   PRODUCT_SKU="vscodium"
+  PRODUCT_UPGRADE_CODE="965370CD-253C-4720-82FC-2E6B02A53808"
   ICON_DIR="..\\..\\..\\src\\stable\\resources\\win32"
 fi
 
@@ -43,7 +45,9 @@ else
    export PLATFORM="${VSCODE_ARCH}"
 fi
 
-sed -i "s|@@PRODUCT_NAME@@|${PRODUCT_NAME}|g" .\\i18n\\*.wsl
+sed -i "s|@@PRODUCT_UPGRADE_CODE@@|${PRODUCT_UPGRADE_CODE}|g" .\\includes\\vscodium-variables.wxi
+
+find i18n -name '*.wxl' -exec sed -i "s|@@PRODUCT_NAME@@|${PRODUCT_NAME}|g" {} \;
 
 BuildSetupTranslationTransform() {
 	local CULTURE=${1}
