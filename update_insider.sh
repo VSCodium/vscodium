5f3af3920b2355d2a0b3c606d6cded28b1c5f31a
Baptiste Augrain <daiyam@zokugun.org>
2022-08-30 15:17:00 +0200

    ci(spearhead): add more details [skip ci]

--- update_insider.sh
+++ update_insider.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 if [[ "${SHOULD_BUILD}" != "yes" ]]; then
   echo "Will not update version JSON because we did not build"
@@ -14,14 +14,19 @@ fi
 
 echo "$( cat "insider.json" | jq --arg 'tag' "${MS_TAG}" --arg 'commit' "${MS_COMMIT}" '. | .tag=$tag | .commit=$commit' )" > "insider.json"
 
-git config user.email "vscodium-ci@not-real.com"
-git config user.name "VSCodium CI"
+git status
+
+git config --global user.email "vscodium-ci@not-real.com"
+git config --global user.name "VSCodium CI"
 git add .
 
 CHANGES=$( git status --porcelain )
 
 if [[ ! -z "${CHANGES}" ]]; then
   git commit -m "build(insider): update to commit ${MS_COMMIT:0:7}"
+
+  git status
+
   if ! git push origin insider --quiet; then
     git pull origin insider
     git push origin insider --quiet
