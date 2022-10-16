e14a115b0ed8235e78bd492af39ded0eaca74831
Baptiste Augrain <daiyam@zokugun.org>
2022-10-16 23:55:34 +0200

    fix: use RELEASE_VERSION for BUILD_SOURCEVERSION (#1293)

--- version.sh
+++ version.sh
@@ -2,24 +2,18 @@
 
 if [[ -z "${BUILD_SOURCEVERSION}" ]]; then
 
-    APP_HASH=$( git rev-parse HEAD )
-
-    cd vscode
-    VSCODE_HASH=$( git rev-parse HEAD )
-    cd ..
-
     if type -t "sha1sum" > /dev/null 2>&1; then
-      export BUILD_SOURCEVERSION=$( echo "${APP_HASH}:${VSCODE_HASH}" | sha1sum | cut -d' ' -f1 )
+      export BUILD_SOURCEVERSION=$( echo "${RELEASE_VERSION}" | sha1sum | cut -d' ' -f1 )
     else
       npm install -g checksum
 
-      export BUILD_SOURCEVERSION=$( echo "${APP_HASH}:${VSCODE_HASH}" | checksum )
+      export BUILD_SOURCEVERSION=$( echo "${RELEASE_VERSION}" | checksum )
     fi
 
     echo "BUILD_SOURCEVERSION=\"${BUILD_SOURCEVERSION}\""
 
     # for GH actions
-    if [[ $GITHUB_ENV ]]; then
-        echo "BUILD_SOURCEVERSION=$BUILD_SOURCEVERSION" >> $GITHUB_ENV
+    if [[ "${GITHUB_ENV}" ]]; then
+        echo "BUILD_SOURCEVERSION=${BUILD_SOURCEVERSION}" >> "${GITHUB_ENV}"
     fi
 fi
