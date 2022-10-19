79fe5639f309ebd3402479688c03988a50addc2c
Baptiste Augrain <daiyam@zokugun.org>
2022-10-19 19:41:51 +0200

    fix: update patch [skip ci]

--- version.sh
+++ version.sh
@@ -3,11 +3,11 @@
 if [[ -z "${BUILD_SOURCEVERSION}" ]]; then
 
     if type -t "sha1sum" > /dev/null 2>&1; then
-      export BUILD_SOURCEVERSION=$( echo "${RELEASE_VERSION}" | sha1sum | cut -d' ' -f1 )
+      export BUILD_SOURCEVERSION=$( echo "${RELEASE_VERSION/-*/}" | sha1sum | cut -d' ' -f1 )
     else
       npm install -g checksum
 
-      export BUILD_SOURCEVERSION=$( echo "${RELEASE_VERSION}" | checksum )
+      export BUILD_SOURCEVERSION=$( echo "${RELEASE_VERSION/-*/}" | checksum )
     fi
 
     echo "BUILD_SOURCEVERSION=\"${BUILD_SOURCEVERSION}\""
