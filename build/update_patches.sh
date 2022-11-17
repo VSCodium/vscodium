1bfa2d920cb894b207937add98bf8cec27661502
Baptiste Augrain <daiyam@zokugun.org>
2022-11-17 22:56:46 +0100

    fix: update patch

--- build/update_patches.sh
+++ build/update_patches.sh
@@ -5,15 +5,16 @@ cd vscode || { echo "'vscode' dir not found"; exit 1; }
 git add .
 git reset -q --hard HEAD
 
-for file in ../patches/*.patch; do
-  if [ -f "${file}" ]; then
-    echo applying patch: "${file}"
-    git apply --ignore-whitespace "${file}"
+for FILE in ../patches/*.patch; do
+  if [ -f "${FILE}" ]; then
+    echo applying patch: "${FILE}"
+    git apply --ignore-whitespace "${FILE}"
     if [ $? -ne 0 ]; then
-      echo failed to apply patch "${file}"
-      git apply --reject "${file}"
+      echo failed to apply patch "${FILE}"
+      git apply --reject "${FILE}"
       read -p "Press any key when the conflict have been resolved..." -n1 -s
-      git diff -U1 > "${file}"
+      git add .
+      git diff --staged -U1 > "${FILE}"
     fi
     git add .
     git reset -q --hard HEAD
