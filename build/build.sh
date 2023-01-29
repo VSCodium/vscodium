553b29623c73d77af34a057aaa5bd3a95e08992d
Baptiste Augrain <daiyam@zokugun.org>
2023-01-29 16:34:54 +0100

    fix: remove patch [skip ci]

--- build/build.sh
+++ build/build.sh
@@ -7,8 +7,9 @@
 export APP_NAME="VSCodium"
 export CI_BUILD="no"
 export SHOULD_BUILD="yes"
-export SKIP_BUILD="no"
 export SKIP_ASSETS="yes"
+export SKIP_BUILD="no"
+export SKIP_SOURCE="no"
 export VSCODE_LATEST="no"
 export VSCODE_QUALITY="stable"
 
@@ -25,6 +26,9 @@ while getopts ":ilop" opt; do
       ;;
     p)
       export SKIP_ASSETS="no"
+      ;;
+    s)
+      export SKIP_SOURCE="yes"
       ;;
   esac
 done
@@ -50,13 +54,14 @@ else
 fi
 
 echo "OS_NAME=\"${OS_NAME}\""
+echo "SKIP_SOURCE=\"${SKIP_SOURCE}\""
 echo "SKIP_BUILD=\"${SKIP_BUILD}\""
 echo "SKIP_ASSETS=\"${SKIP_ASSETS}\""
 echo "VSCODE_ARCH=\"${VSCODE_ARCH}\""
 echo "VSCODE_LATEST=\"${VSCODE_LATEST}\""
 echo "VSCODE_QUALITY=\"${VSCODE_QUALITY}\""
 
-if [[ "${SKIP_BUILD}" == "no" ]]; then
+if [[ "${SKIP_SOURCE}" == "no" ]]; then
   rm -rf vscode* VSCode*
 
   . get_repo.sh
@@ -67,19 +72,34 @@ if [[ "${SKIP_BUILD}" == "no" ]]; then
   echo "MS_COMMIT=\"${MS_COMMIT}\"" >> build.env
   echo "RELEASE_VERSION=\"${RELEASE_VERSION}\"" >> build.env
   echo "BUILD_SOURCEVERSION=\"${BUILD_SOURCEVERSION}\"" >> build.env
-
-  . build.sh
-
-  if [[ "${VSCODE_QUALITY}" == "insider" && "${VSCODE_LATEST}" == "yes" ]]; then
-    echo "$( cat "insider.json" | jq --arg 'tag' "${MS_TAG/\-insider/}" --arg 'commit' "${MS_COMMIT}" '. | .tag=$tag | .commit=$commit' )" > "insider.json"
-  fi
 else
+  if [[ "${SKIP_ASSETS}" != "no" ]]; then
+    rm -rf VSCode*
+  fi
+
   . build.env
 
   echo "MS_TAG=\"${MS_TAG}\""
   echo "MS_COMMIT=\"${MS_COMMIT}\""
   echo "RELEASE_VERSION=\"${RELEASE_VERSION}\""
   echo "BUILD_SOURCEVERSION=\"${BUILD_SOURCEVERSION}\""
+fi
+
+if [[ "${SKIP_BUILD}" == "no" ]]; then
+  if [[ "${SKIP_SOURCE}" != "no" ]]; then
+    cd vscode || { echo "'vscode' dir not found"; exit 1; }
+
+    git add .
+    git reset -q --hard HEAD
+
+    cd ..
+  fi
+
+  . build.sh
+
+  if [[ "${VSCODE_QUALITY}" == "insider" && "${VSCODE_LATEST}" == "yes" ]]; then
+    echo "$( cat "insider.json" | jq --arg 'tag' "${MS_TAG/\-insider/}" --arg 'commit' "${MS_COMMIT}" '. | .tag=$tag | .commit=$commit' )" > "insider.json"
+  fi
 fi
 
 if [[ "${SKIP_ASSETS}" == "no" ]]; then
