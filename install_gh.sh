fc770788681fab2079ee60fe3ad13c979a5dd22d
Baptiste Augrain <daiyam@zokugun.org>
2023-01-31 17:13:55 +0100

    fix: remove retry-all-errors option [skip ci]

--- install_gh.sh
+++ install_gh.sh
@@ -4,9 +4,9 @@ set -ex
 
 GH_ARCH="amd64"
 
-VERSION=`curl --retry 12 --retry-delay 30 --retry-all-errors "https://api.github.com/repos/cli/cli/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c2-`
+VERSION=`curl --retry 12 --retry-delay 30 "https://api.github.com/repos/cli/cli/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c2-`
 
-curl --retry 12 --retry-delay 120 --retry-all-errors -sSL "https://github.com/cli/cli/releases/download/v${VERSION}/gh_${VERSION}_linux_${GH_ARCH}.tar.gz" -o "gh_${VERSION}_linux_${GH_ARCH}.tar.gz"
+curl --retry 12 --retry-delay 120 -sSL "https://github.com/cli/cli/releases/download/v${VERSION}/gh_${VERSION}_linux_${GH_ARCH}.tar.gz" -o "gh_${VERSION}_linux_${GH_ARCH}.tar.gz"
 
 tar xf "gh_${VERSION}_linux_${GH_ARCH}.tar.gz"
 
