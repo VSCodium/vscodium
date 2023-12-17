6ada8c9026e0560417ade5c788103f56d9588fd6
Baptiste Augrain <daiyam@zokugun.org>
2023-12-17 19:41:35 +0100

    ci: add user/email for merge [skip ci]

--- get_repo.sh
+++ get_repo.sh
@@ -11,10 +11,12 @@ fi
 if [[ -n "${PULL_REQUEST_ID}" ]]; then
   BRANCH_NAME=$( git rev-parse --abbrev-ref HEAD )
 
+  git config --global user.email "$( echo "${GITHUB_USERNAME}" | awk '{print tolower($0)}' )-ci@not-real.com"
+  git config --global user.name "${GITHUB_USERNAME} CI"
   git fetch --unshallow
   git fetch origin "pull/${PULL_REQUEST_ID}/head"
   git checkout FETCH_HEAD
-  git merge --no-ff --no-commit "origin/${BRANCH_NAME}"
+  git merge --no-edit "origin/${BRANCH_NAME}"
 fi
 
 if [[ -z "${RELEASE_VERSION}" ]]; then
