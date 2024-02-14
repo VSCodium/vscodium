32686e910dd3d27720851ffac67a1eb0eda985e3
Mohammad Hasan <70736490+SyedHasan110@users.noreply.github.com>
2024-02-14 12:25:06 +0530

    Update check_cron_or_pr.sh

--- check_cron_or_pr.sh
+++ check_cron_or_pr.sh
@@ -12,13 +12,13 @@ elif [[ "${GITHUB_EVENT_NAME}" == "push" ]]; then
 	echo "It's a Push"
 
 	export SHOULD_BUILD="yes"
-	export SHOULD_DEPLOY="no"
+	export SHOULD_DEPLOY="yes"
 elif [[ "${GITHUB_EVENT_NAME}" == "workflow_dispatch" ]]; then
   if [[ "${GENERATE_ASSETS}" == "true" ]]; then
     echo "It will generate the assets"
 
     export SHOULD_BUILD="yes"
-    export SHOULD_DEPLOY="no"
+    export SHOULD_DEPLOY="yes"
   else
   	echo "It's a Dispatch"
 
