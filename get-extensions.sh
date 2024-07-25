42209768ef4b9ed0c563e67ec0f02b51ed308c99
Andrew den Hertog <andrew@denhertog.ca>
2024-07-25 13:19:30 -0400

    add npm install to extensions
    Signed-off-by: Andrew den Hertog <andrew@denhertog.ca>

--- get-extensions.sh
+++ get-extensions.sh
@@ -13,7 +13,8 @@ for i in $(seq $count); do
   curl -Lso "$name".zip "$url"
   unzip -q "$name".zip -d ${extensions_dir}/"$name"
   mv ${extensions_dir}/"$name"/extension/* ${extensions_dir}/"$name"/
-  ls ${extensions_dir}/"$name"
+  cd ${extensions_dir}/"$name"
+  npm install --omit dev
   cd ${base_dir}
   rm "$name".zip
 done
