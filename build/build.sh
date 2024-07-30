4043b2eb3458db0e366af2df8f7da309b42623ae
Andrew den Hertog <andrew@denhertog.ca>
2024-07-29 21:08:52 -0400

    fix some more conflicts
    Signed-off-by: Andrew den Hertog <andrew@denhertog.ca>

--- build/build.sh
+++ build/build.sh
@@ -120,14 +120,11 @@ if [[ "${SKIP_ASSETS}" == "no" ]]; then
     rm -rf build/windows/msi/releasedir
   fi
 
-<<<<<<< HEAD
   if [[ "${OS_NAME}" == "osx" && -f "./macos-codesign.env" ]]; then
     . macos-codesign.env
 
     echo "CERTIFICATE_OSX_ID: ${CERTIFICATE_OSX_ID}"
   fi
 
-=======
->>>>>>> f1f09ccc112c37e3e9134eef3db964ada7ecb951
   . prepare_assets.sh
 fi
--- stores/snapcraft/check_version.sh
+++ stores/snapcraft/check_version.sh
@@ -7,7 +7,6 @@ export SHOULD_BUILD="no"
 export SHOULD_DEPLOY_TO_RELEASE="no"
 export SHOULD_DEPLOY_TO_STORE="no"
 
-<<<<<<< HEAD
 # Support for GitHub Enterprise
 GH_HOST="${GH_HOST:-github.com}"
 
@@ -15,12 +14,6 @@ if [[ "${GENERATE_ASSETS}" == "true" ]]; then
   export SHOULD_BUILD="yes"
 else
   wget --quiet "https://api.${GH_HOST}/repos/${ASSETS_REPOSITORY}/releases" -O gh_latest.json
-=======
-if [[ "${GENERATE_ASSETS}" == "true" ]]; then
-  export SHOULD_BUILD="yes"
-else
-  wget --quiet "https://api.github.com/repos/${ASSETS_REPOSITORY}/releases" -O gh_latest.json
->>>>>>> f1f09ccc112c37e3e9134eef3db964ada7ecb951
   SNAP_URL=$( jq -r 'map(select(.tag_name == "'"${RELEASE_VERSION}"'"))|first.assets[].browser_download_url|select(endswith("'"_${ARCHITECTURE}.snap"'"))' gh_latest.json )
 
   if [[ -z "${SNAP_URL}" ]]; then
