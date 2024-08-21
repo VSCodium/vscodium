9ac1a6a4f6ffa9aa6385e18be3a127684e75cda1
Andrew den Hertog <andrew@denhertog.ca>
2024-08-21 10:50:56 -0400

    fix extensions script
    Signed-off-by: Andrew den Hertog <andrew@denhertog.ca>

--- get-extensions.sh
+++ get-extensions.sh
@@ -9,10 +9,20 @@ for i in $(seq $count); do
   url=$(jq -r ".[$i-1].url" ${jsonfile})
   name=$(jq -r ".[$i-1].name" ${jsonfile})
   echo $name $url
+  if [[ -d ${extensions_dir}/"$name" ]]; then
+    rm -rf ${extensions_dir}/"$name"
+  fi
   mkdir -p ${extensions_dir}/"$name"
   curl -Lso "$name".zip "$url"
   unzip -q "$name".zip -d ${extensions_dir}/"$name"
   mv ${extensions_dir}/"$name"/extension/* ${extensions_dir}/"$name"/
+  if [[ "${name}" == "codex-scripture-viewer" ]]; then
+    cd ${extensions_dir}/"$name"
+    npm prune --omit=dev
+    cd ./webviews/codex-webviews
+    npm prune --omit=dev
+    cd ${base_dir}
+  fi
   rm "$name".zip
 done
 
