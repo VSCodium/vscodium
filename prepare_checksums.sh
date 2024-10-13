a6cd884c375e51d1245303e1334101fa9321725a
Baptiste Augrain <daiyam@zokugun.org>
2024-10-14 01:03:50 +0200

    build(windows): calculate checksums after signing [skip ci]

---
+++ prepare_checksums.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+npm install -g checksum
+
+sum_file() {
+  if [[ -f "${1}" ]]; then
+    echo "Calculating checksum for ${1}"
+    checksum -a sha256 "${1}" > "${1}".sha256
+    checksum "${1}" > "${1}".sha1
+  fi
+}
+
+cd assets
+
+for FILE in *; do
+  if [[ -f "${FILE}" ]]; then
+    sum_file "${FILE}"
+  fi
+done
+
+cd ..
