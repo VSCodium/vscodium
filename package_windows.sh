7e79fdc30364c85e7f852d556443434092fa7d8c
Baptiste Augrain <daiyam@zokugun.org>
2024-11-02 12:51:21 +0100

    build(windows): pre-compile on linux (#2080)

---
+++ package_windows.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1091
+
+set -ex
+
+if [[ "${CI_BUILD}" == "no" ]]; then
+  exit 1
+fi
+
+tar -xzf ./vscode.tar.gz
+
+cd vscode || { echo "'vscode' dir not found"; exit 1; }
+
+for i in {1..5}; do # try 5 times
+  npm ci && break
+  if [[ $i -eq 3 ]]; then
+    echo "Npm install failed too many times" >&2
+    exit 1
+  fi
+  echo "Npm install failed $i, trying again..."
+done
+
+node build/azure-pipelines/distro/mixin-npm
+
+. ../build/windows/rtf/make.sh
+
+yarn gulp "vscode-win32-${VSCODE_ARCH}-min-ci"
+
+cd ..
