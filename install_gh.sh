292d2783db6b59c68298d917e99dd370ed8fbbe7
Thibaud CANALE <thican@users.noreply.github.com>
2024-12-21 01:38:49 +0100

    ci: use jq to get gh cli (#2168)
    Signed-off-by: Thibaud CANALE <thican@thican.net>

--- install_gh.sh
+++ install_gh.sh
@@ -4,9 +4,10 @@ set -ex
 
 GH_ARCH="amd64"
 
-VERSION=$( curl --retry 12 --retry-delay 30 "https://api.github.com/repos/cli/cli/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c2- )
+TAG=$( curl --retry 12 --retry-delay 30 "https://api.github.com/repos/cli/cli/releases/latest" | jq --raw-output '.tag_name' )
+VERSION=${TAG#v}
 
-curl --retry 12 --retry-delay 120 -sSL "https://github.com/cli/cli/releases/download/v${VERSION}/gh_${VERSION}_linux_${GH_ARCH}.tar.gz" -o "gh_${VERSION}_linux_${GH_ARCH}.tar.gz"
+curl --retry 12 --retry-delay 120 -sSL "https://github.com/cli/cli/releases/download/${TAG}/gh_${VERSION}_linux_${GH_ARCH}.tar.gz" -o "gh_${VERSION}_linux_${GH_ARCH}.tar.gz"
 
 tar xf "gh_${VERSION}_linux_${GH_ARCH}.tar.gz"
 
