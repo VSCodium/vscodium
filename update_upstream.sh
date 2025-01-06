097441843d63efc3b206997022439682e99222af
Baptiste Augrain <daiyam@zokugun.org>
2025-01-06 16:49:04 +0100

    ci: remove token check [skip ci]

--- update_upstream.sh
+++ update_upstream.sh
@@ -8,11 +8,6 @@ if [[ "${SHOULD_BUILD}" != "yes" ]]; then
   exit 0
 fi
 
-if [[ -z "${GH_TOKEN}" ]] && [[ -z "${GITHUB_TOKEN}" ]] && [[ -z "${GH_ENTERPRISE_TOKEN}" ]] && [[ -z "${GITHUB_ENTERPRISE_TOKEN}" ]]; then
-  echo "Will not update ${VSCODE_QUALITY}.json because no GITHUB_TOKEN defined"
-  exit 0
-fi
-
 jsonTmp=$( cat "./upstream/${VSCODE_QUALITY}.json" | jq --arg 'tag' "${MS_TAG/\-insider/}" --arg 'commit' "${MS_COMMIT}" '. | .tag=$tag | .commit=$commit' )
 echo "${jsonTmp}" > "./upstream/${VSCODE_QUALITY}.json" && unset jsonTmp
 
