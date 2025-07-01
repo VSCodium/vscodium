86a2d8399912db97945fa2e0d3c4a5469f6ddbc7
Baptiste Augrain <daiyam@zokugun.org>
2025-07-01 14:51:06 +0200

    fix: stop install on error

--- install_gh.sh
+++ install_gh.sh
@@ -4,8 +4,14 @@ set -ex
 
 GH_ARCH="amd64"
 
-TAG=$( curl --retry 12 --retry-delay 30 "https://api.github.com/repos/cli/cli/releases/latest" | jq --raw-output '.tag_name' )
-VERSION=${TAG#v}
+TAG=$( curl --retry 12 --retry-delay 30 "https://api.github.com/repos/cli/cli/releases/latest" 2>/dev/null | jq --raw-output '.tag_name' )
+
+if [[ $? -ne 0 ]] || [[ -z "${TAG}" ]]; then
+  echo "Error: Failed to retrieve the latest tag from GitHub CLI releases."
+  exit 1
+fi
+
+VERSION="${TAG#v}"
 
 curl --retry 12 --retry-delay 120 -sSL "https://github.com/cli/cli/releases/download/${TAG}/gh_${VERSION}_linux_${GH_ARCH}.tar.gz" -o "gh_${VERSION}_linux_${GH_ARCH}.tar.gz"
 
