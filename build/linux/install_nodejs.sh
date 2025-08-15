e9ebc98e9dad201d541155b261c5b9c399b7f79a
Baptiste Augrain <daiyam@zokugun.org>
2025-08-15 10:35:57 +0200

    fix: typo suffix var

--- build/linux/install_nodejs.sh
+++ build/linux/install_nodejs.sh
@@ -4,7 +4,7 @@ set -ex
 
 NODEJS_VERSION=$( cat .nvmrc )
 
-curl --silent --fail -L -O "${NODEJS_SITE}${NODEJS_URLROOT}/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}${NODE_URLSUFFIX}.tar.gz"
+curl --silent --fail -L -O "${NODEJS_SITE}${NODEJS_URLROOT}/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}${NODEJS_URLSUFFIX}.tar.gz"
 
 tar -xzf "node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}.tar.gz"
 
