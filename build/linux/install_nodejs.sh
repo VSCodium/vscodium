df717b0c4c2bc0c42631ccf7418da7d221dad9eb
Baptiste Augrain <daiyam@zokugun.org>
2025-08-15 11:05:18 +0200

    fix: add suffix to path

--- build/linux/install_nodejs.sh
+++ build/linux/install_nodejs.sh
@@ -6,10 +6,10 @@ NODEJS_VERSION=$( cat .nvmrc )
 
 curl --silent --fail -L -O "${NODEJS_SITE}${NODEJS_URLROOT}/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}${NODEJS_URLSUFFIX}.tar.gz"
 
-tar -xzf "node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}.tar.gz"
+tar -xzf "node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}${NODEJS_URLSUFFIX}.tar.gz"
 
-cp -R node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}/* /usr/local/
+cp -R node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}${NODEJS_URLSUFFIX}/* /usr/local/
 
-rm -rf node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}*
+rm -rf node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}${NODEJS_URLSUFFIX}*
 
 node --version
