0ea62bcdb2493a5353f77af7da144a57161860d7
Baptiste Augrain <daiyam@zokugun.org>
2025-08-15 10:00:34 +0200

    fix: add parameters to indicate where to download nodejs

---
+++ build/linux/install_nodejs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -ex
+
+NODEJS_VERSION=$( cat .nvmrc )
+
+curl --silent --fail -L -O "${NODEJS_URLROOT}/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}${NODE_URLSUFFIX}.tar.gz"
+
+tar -xzf "node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}.tar.gz"
+
+cp -R node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}/* /usr/local/
+
+rm -rf node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}*
+
+node --version
