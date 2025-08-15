98b17073506b595072f0700cd0eb0e4070f32d64
Baptiste Augrain <daiyam@zokugun.org>
2025-08-15 11:32:33 +0200

    fix: move with sudo and add to env path

--- build/linux/install_nodejs.sh
+++ build/linux/install_nodejs.sh
@@ -4,12 +4,10 @@ set -ex
 
 NODEJS_VERSION=$( cat .nvmrc )
 
-curl --silent --fail -L -O "${NODEJS_SITE}${NODEJS_URLROOT}/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}${NODEJS_URLSUFFIX}.tar.gz"
+curl -fsSL "${NODEJS_SITE}${NODEJS_URLROOT}/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}${NODEJS_URLSUFFIX}.tar.xz" -o node.tar.xz
 
-tar -xzf "node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}${NODEJS_URLSUFFIX}.tar.gz"
+tar -xf node.tar.xz
 
-cp -R node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}${NODEJS_URLSUFFIX}/* /usr/local/
+sudo mv "node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}${NODEJS_URLSUFFIX}" /usr/local/node
 
-rm -rf node-v${NODEJS_VERSION}-linux-${NODEJS_ARCH}${NODEJS_URLSUFFIX}*
-
-node --version
+echo "/usr/local/node/bin" >> $GITHUB_PATH
