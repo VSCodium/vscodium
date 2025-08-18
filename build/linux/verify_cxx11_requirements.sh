5a54e3fdc4ab6a014d363f4aa8c66315640ac9a2
Baptiste Augrain <daiyam@zokugun.org>
2025-08-18 14:17:07 +0200

    fix: add missing closing quote

--- build/linux/verify_cxx11_requirements.sh
+++ build/linux/verify_cxx11_requirements.sh
@@ -8,7 +8,7 @@ echo "Verifying requirements for files: ${FILES}"
 
 for FILE in ${FILES}; do
   if [[ -n "$( strings "${FILE}" | grep cxx11 | tail -n1 )" ]]; then
-    echo "Error: File ${FILE} has dependency on CXX11
+    echo "Error: File ${FILE} has dependency on CXX11"
     exit 1
   fi
 done
