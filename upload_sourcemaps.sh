c0ac4bd72e7e81d66f481e7920655f6d8a90c1c7
Baptiste Augrain <daiyam@zokugun.org>
2025-10-09 14:10:02 +0200

    fix(sourcemap): add quality to sourcemaps release [skip ci]

--- upload_sourcemaps.sh
+++ upload_sourcemaps.sh
@@ -33,27 +33,28 @@ checksum -a sha256 checksum.txt > checksum.txt.sha256
 
 REPOSITORY_OWNER="${SOURCEMAPS_REPOSITORY/\/*/}"
 REPOSITORY_NAME="${SOURCEMAPS_REPOSITORY/*\//}"
+RELEASE_TAG="${VSCODE_QUALITY}-${BUILD_SOURCEVERSION}"
 
-gh release create "${BUILD_SOURCEVERSION}" --repo "${SOURCEMAPS_REPOSITORY}" --title "${RELEASE_VERSION}"
+gh release create "${RELEASE_TAG}" --repo "${SOURCEMAPS_REPOSITORY}" --title "${RELEASE_VERSION}"
 
 set +e
 
 for FILE in *; do
   if [[ -f "${FILE}" ]]; then
     echo "::group::Uploading '${FILE}' at $( date "+%T" )"
-    gh release upload --repo "${SOURCEMAPS_REPOSITORY}" "${RELEASE_VERSION}" "${FILE}"
+    gh release upload --repo "${SOURCEMAPS_REPOSITORY}" "${RELEASE_TAG}" "${FILE}"
 
     EXIT_STATUS=$?
     echo "exit: ${EXIT_STATUS}"
 
     if (( "${EXIT_STATUS}" )); then
       for (( i=0; i<10; i++ )); do
-        github-release delete --owner "${REPOSITORY_OWNER}" --repo "${REPOSITORY_NAME}" --tag "${BUILD_SOURCEVERSION}" "${FILE}"
+        github-release delete --owner "${REPOSITORY_OWNER}" --repo "${REPOSITORY_NAME}" --tag "${RELEASE_TAG}" "${FILE}"
 
         sleep $(( 15 * (i + 1)))
 
         echo "RE-Uploading '${FILE}' at $( date "+%T" )"
-        gh release upload --repo "${SOURCEMAPS_REPOSITORY}" "${BUILD_SOURCEVERSION}" "${FILE}"
+        gh release upload --repo "${SOURCEMAPS_REPOSITORY}" "${RELEASE_TAG}" "${FILE}"
 
         EXIT_STATUS=$?
         echo "exit: ${EXIT_STATUS}"
@@ -67,7 +68,7 @@ for FILE in *; do
       if (( "${EXIT_STATUS}" )); then
         echo "'${FILE}' hasn't been uploaded!"
 
-        github-release delete --owner "${REPOSITORY_OWNER}" --repo "${REPOSITORY_NAME}" --tag "${BUILD_SOURCEVERSION}" "${FILE}"
+        github-release delete --owner "${REPOSITORY_OWNER}" --repo "${REPOSITORY_NAME}" --tag "${RELEASE_TAG}" "${FILE}"
 
         exit 1
       fi
