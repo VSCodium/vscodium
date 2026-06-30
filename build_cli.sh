67325b0375e0c3fb75e12169238deaf1c9c848f6
Michael Baumgartner <Michael-Baumgartner@posteo.de>
2026-06-30 18:21:35 +0200

    Fix EditorConfig violations in Shell scripts

--- build_cli.sh
+++ build_cli.sh
@@ -62,7 +62,7 @@ else
 
   if [[ "${VSCODE_ARCH}" == "arm64" ]]; then
     VSCODE_CLI_TARGET="aarch64-unknown-linux-gnu"
-    
+
     if [[ "${CI_BUILD}" != "no" ]]; then
       export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
       export CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
--- check_cron_or_pr.sh
+++ check_cron_or_pr.sh
@@ -4,15 +4,15 @@
 set -e
 
 if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
-	echo "It's a PR"
+  echo "It's a PR"
 
   export SHOULD_BUILD="yes"
-	export SHOULD_DEPLOY="no"
+  export SHOULD_DEPLOY="no"
 elif [[ "${GITHUB_EVENT_NAME}" == "push" ]]; then
-	echo "It's a Push"
+  echo "It's a Push"
 
-	export SHOULD_BUILD="yes"
-	export SHOULD_DEPLOY="no"
+  export SHOULD_BUILD="yes"
+  export SHOULD_DEPLOY="no"
 elif [[ "${GITHUB_EVENT_NAME}" == "workflow_dispatch" ]]; then
   if [[ "${GENERATE_ASSETS}" == "true" ]]; then
     echo "It will generate the assets"
@@ -20,14 +20,14 @@ elif [[ "${GITHUB_EVENT_NAME}" == "workflow_dispatch" ]]; then
     export SHOULD_BUILD="yes"
     export SHOULD_DEPLOY="no"
   else
-  	echo "It's a Dispatch"
+    echo "It's a Dispatch"
 
     export SHOULD_DEPLOY="yes"
   fi
 else
-	echo "It's a Cron"
+  echo "It's a Cron"
 
-	export SHOULD_DEPLOY="yes"
+  export SHOULD_DEPLOY="yes"
 fi
 
 if [[ "${GITHUB_ENV}" ]]; then
