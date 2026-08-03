5ee846294bf2ece6a13faa167bbe061e485b6e5b
Shikakiben <shikakiben@users.noreply.github.com>
2026-08-03 18:07:57 +0200

    feat: add anylinux-appimage build (quick-sharun + archlinux)
    - Add anylinux-appimage job in ci-build-linux.yml (x86_64 + aarch64)
    - Use pkgforge-dev/anylinux-setup-action + quick-sharun
    - Build AppImage from VSCode-linux-x64 artifact via get-dependencies.sh + make-appimage.sh
    - Add test-anylinux-appimage.yml for standalone testing
    - Old pkg2appimage build remains untouched

---
+++ build/anylinux-appimage/make-appimage.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+QUALITY="${VSCODE_QUALITY:-stable}"
+export ARCH
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON="icons/$QUALITY/codium_cnl.svg"
+export DEPLOY_GTK=1
+export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
+
+# Deploy dependencies
+quick-sharun ./AppDir/bin/*
+
+# Additional changes can be done in between here
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
+
+# Test the AppImage (simple test: check for missing symbols)
+quick-sharun --simple-test ./dist/*.AppImage
