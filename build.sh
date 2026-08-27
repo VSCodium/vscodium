0dd32fe43a7b2c9b9f5e436e4af5d0839bb0497f
Baptiste Augrain <daiyam@zokugun.org>
2026-08-27 21:14:24 +0200

    wip: fixing oom and slow build

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ if [[ "${SHOULD_BUILD}" == "yes" ]]; then
 
   cd vscode || { echo "'vscode' dir not found"; exit 1; }
 
-  export NODE_OPTIONS="--max-old-space-size=8192"
+  export NODE_OPTIONS="--max-old-space-size=12288"
   export VSCODE_PUBLISH_COUNTER=1
 
   npm run gulp vscode-min-prepack
