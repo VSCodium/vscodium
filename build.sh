36fef3ae77aad05e80163c30b17d891b51909c1c
Baptiste Augrain <daiyam@zokugun.org>
2026-08-28 17:36:32 +0200

    fix: replace max-old-space-size with our own limit (#2979)

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ set -ex
 if [[ "${SHOULD_BUILD}" == "yes" ]]; then
   echo "MS_COMMIT=\"${MS_COMMIT}\""
 
+  export MAX_OLD_SPACE_SIZE="${MAX_OLD_SPACE_SIZE:-8192}"
+
   . prepare_vscode.sh
 
   cd vscode || { echo "'vscode' dir not found"; exit 1; }
-
-  MAX_OLD_SPACE_SIZE="${MAX_OLD_SPACE_SIZE:-8192}"
 
   export NODE_OPTIONS="--max-old-space-size=${MAX_OLD_SPACE_SIZE}"
   export VSCODE_PUBLISH_COUNTER=1
