2be21b1baf351b5f2d4186f4d3504eb264a1c388
Thad House <ThadHouse@users.noreply.github.com>
2026-08-31 05:52:48 -0700

    enhance(build): don't move the update-disable patch (#2983)

--- prepare_vscode.sh
+++ prepare_vscode.sh
@@ -144,7 +144,7 @@ echo "ORG_NAME=\"${ORG_NAME}\""
 echo "TUNNEL_APP_NAME=\"${TUNNEL_APP_NAME}\""
 
 if [[ "${DISABLE_UPDATE}" == "yes" ]]; then
-  mv ../patches/00-update-disable.patch.yet ../patches/00-update-disable.patch
+  apply_patch ../patches/00-update-disable.patch.yet
 fi
 
 for file in ../patches/*.json; do
