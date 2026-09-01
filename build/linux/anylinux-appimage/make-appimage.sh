f1d486780d886461bc61f125b79de970905393ca
Baptiste Augrain <daiyam@zokugun.org>
2026-09-01 15:41:11 +0200

    fix: AppImage filename

--- build/linux/anylinux-appimage/make-appimage.sh
+++ build/linux/anylinux-appimage/make-appimage.sh
@@ -22,22 +22,20 @@ NAME_LONG=$(jq -r '.nameLong' ./AppDir/bin/resources/app/product.json)
 NAME_SHORT=$(jq -r '.nameShort' ./AppDir/bin/resources/app/product.json)
 NAME=$(jq -r '.applicationName' ./AppDir/bin/resources/app/product.json)
 ICON=$(jq -r '.linuxIconName' ./AppDir/bin/resources/app/product.json)
-QUALITY=$(jq -r '.quality' ./AppDir/bin/resources/app/product.json)
 URLPROTOCOL=$(jq -r '.urlProtocol' ./AppDir/bin/resources/app/product.json)
-VERSION=$(jq -r '.version' ./AppDir/bin/resources/app/product.json)
+VERSION="${RELEASE_VERSION%-insider}"
 
 echo "NAME_LONG=${NAME_LONG}"
 echo "NAME_SHORT=${NAME_SHORT}"
 echo "NAME=${NAME}"
 echo "ICON=${ICON}"
-echo "QUALITY=${QUALITY}"
 echo "URLPROTOCOL=${URLPROTOCOL}"
 echo "VERSION=${VERSION}"
 
 echo "${VERSION}" > ~/version
 
 # Desktop entries from repo sources
-SRC_DIR="src/${QUALITY}/resources/linux"
+SRC_DIR="src/${VSCODE_QUALITY}/resources/linux"
 
 cp "${SRC_DIR}/code.desktop" "./AppDir/${NAME}.desktop"
 cp "${SRC_DIR}/code-url-handler.desktop" "./AppDir/share/applications/${NAME}-url-handler.desktop"
@@ -56,11 +54,17 @@ sed -i \
   "./AppDir/share/applications/${NAME}-url-handler.desktop" \
   ./AppDir/bin/cli-router.hook
 
+if [[ "${VSCODE_QUALITY}" == "insider" ]]; then
+  export OUTNAME=${APP_NAME}-Insiders-${VERSION}-anylinux-"${ARCH}".AppImage
+else
+  export OUTNAME=${APP_NAME}-${VERSION}-anylinux-"${ARCH}".AppImage
+fi
+
 export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*${ARCH}.AppImage.zsync"
-export ICON="src/${QUALITY}/resources/linux/code.svg"
+export ICON="src/${VSCODE_QUALITY}/resources/linux/code.svg"
 export DEPLOY_GTK=1
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
